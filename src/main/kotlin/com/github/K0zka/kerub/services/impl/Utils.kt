83d809fb263a72fba5b3cd6c40cd6239e4700487
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-07-09 22:49:48 +0200

    added some javadoc
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/services/impl/Utils.kt
+++ src/main/kotlin/com/github/K0zka/kerub/services/impl/Utils.kt
@@ -5,6 +5,9 @@ import javax.ws.rs.NotFoundException
 /**
  * Utility function to throw Exception if object does not exist
  * and return safe non-null reference to object if it does
+ * @param entityType    the type name of entity
+ * @param value the entity object
+ * @param id    the ID of the entity
  */
 fun assertExist<T>(entityType : String, value: T?, id: Any): T {
 	if (value == null) {
