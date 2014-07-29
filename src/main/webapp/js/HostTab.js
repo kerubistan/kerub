0b99b2d3be78a0293c6d1e6b955b1a3f7d049f3a
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-07-29 08:18:54 +0200

    added new host dialog - with fake content
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/webapp/js/HostTab.js
+++ src/main/webapp/js/HostTab.js
@@ -1,6 +1,8 @@
-kerubApp.controller('HostTab', function($scope, $http) {
+kerubApp.controller('HostTab', function($scope, $http, $modal) {
     $scope.newHostForm = function() {
-
+        $modal.open({
+            templateUrl : 'NewHostWizard'
+        });
     };
     $http.get('s/r/host.json').success(function(hosts) {
         $scope.hosts = hosts
