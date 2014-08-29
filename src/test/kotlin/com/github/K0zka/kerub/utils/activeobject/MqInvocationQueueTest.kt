42f495d57b11b49253c8ad5ac51881072a3c8a4f
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-08-29 19:53:43 +0200

    minor improvement on Mq invocation queue tests
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/K0zka/kerub/utils/activeobject/MqInvocationQueueTest.kt
+++ src/test/kotlin/com/github/K0zka/kerub/utils/activeobject/MqInvocationQueueTest.kt
@@ -8,15 +8,21 @@ import org.mockito.runners.MockitoJUnitRunner
 import org.mockito.Mockito
 import org.mockito.Matchers
 import org.springframework.jms.core.MessageCreator
+import javax.jms.Session
 
 RunWith(javaClass<MockitoJUnitRunner>())
 public class MqInvocationQueueTest {
 	Mock
 	var template : JmsTemplate? = null
+	Mock
+	var session : Session? = null
 	Test
 	fun send() {
 		val queue = MqInvocationQueue(template!!)
+		Mockito.doAnswer({ (it!!.getArguments()!![0] as MessageCreator).createMessage(session) })!!
+				.`when`(template)!!.send(Matchers.any(javaClass<MessageCreator>()))
 		queue.send(AsyncInvocation("", "", listOf(), listOf()))
 		Mockito.verify(template)!!.send(Matchers.any(javaClass<MessageCreator>()));
+		Mockito.verify(session)!!.createObjectMessage(Matchers.any(javaClass<AsyncInvocation>()))
 	}
 }
