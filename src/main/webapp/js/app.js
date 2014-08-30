852418f40f195f6da0998499f7411b217bee2918
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-08-31 01:16:51 +0200

    added angular router to the config
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,4 @@
-var kerubApp = angular.module('kerubApp', ['ui.bootstrap']);
+var kerubApp = angular.module('kerubApp', ['ui.bootstrap', 'ngRoute']);
 
 kerubApp.controller('Login', function($scope) {
     $scope.login = function($log) {
