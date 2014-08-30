ea1d7f21fade670a8a3450b84b94419406b40a83
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-08-31 00:30:10 +0200

    improved host tab test
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/js/HostTabTest.js
+++ src/test/js/HostTabTest.js
@@ -6,12 +6,15 @@ describe('HostTab', function() {
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
         $httpBackend = _$httpBackend_;
         $httpBackend.expectGET('s/r/host')
-            .respond([]);
+            .respond([{address: '127.0.0.1'}]);
         scope = $rootScope.$new();
         ctrl = $controller('HostTab', {$scope : scope});
     }));
 
     it('should load the list of hosts', function() {
-
+        expect(scope.hosts).toBeUndefined();
+        $httpBackend.flush();
+        expect(scope.hosts.length).toBe(1);
+        expect(scope.hosts[0].address).toBe('127.0.0.1');
     });
 });
