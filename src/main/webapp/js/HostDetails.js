763af9889f4b2e045776aad6d2dad2e61b7e3c16
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-06-08 06:46:11 +0200

    New tabs for the host details modal
    Bug-Url: https://github.com/K0zka/kerub/issues/24
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/webapp/js/HostDetails.js
+++ src/main/webapp/js/HostDetails.js
@@ -1,10 +1,30 @@
 var HostDetails = function($scope, $log, $modalInstance, $appsession, hostId) {
     $scope.host = null;
 
+    $scope.tab = "overview";
+
     $appsession
         .get('s/r/host/'+hostId)
         .success(function(host) {
             $log.debug('host details received');
             $scope.host = host;
-        })
+        });
+
+    $scope.selectView = function(view) {
+        $log.info('select view ', view);
+        $scope.tab = view;
+    };
+
+    $scope.isActive = function(view) {
+        if($scope.tab === view) {
+            return 'active'
+        } else {
+            return '';
+        }
+    }
+
+    $scope.isVisible = function(view) {
+        return $scope.tab === view
+    }
+
 }
