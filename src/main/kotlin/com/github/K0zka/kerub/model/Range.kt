f48759ca8a57f3e3cb1eaeae9f28b5a6cc27b702
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-06-17 08:29:23 +0200

    Update Range.kt

--- src/main/kotlin/com/github/K0zka/kerub/model/Range.kt
+++ src/main/kotlin/com/github/K0zka/kerub/model/Range.kt
@@ -3,5 +3,8 @@ package com.github.K0zka.kerub.model
 import javax.xml.bind.annotation.XmlRootElement
 import java.io.Serializable
 
+/**
+ * A serializable range.
+ */
 XmlRootElement(name = "range")
 data class Range<T> (final val min : T, final val max: T) : Serializable
