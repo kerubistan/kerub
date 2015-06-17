575abc3bf642dcd7cad4c229770e9a1505e8c524
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-06-17 08:52:02 +0200

    Update Expectation.kt

--- src/main/kotlin/com/github/K0zka/kerub/model/Expectation.kt
+++ src/main/kotlin/com/github/K0zka/kerub/model/Expectation.kt
@@ -3,6 +3,10 @@ package com.github.K0zka.kerub.model
 import java.util.UUID
 import java.io.Serializable
 
+/**
+ * Base expectation interface.
+ * Expectations describe SLA on virtual resources.
+ */
 data interface Expectation: Serializable {
 	val id : UUID
 	val level : ExpectationLevel
