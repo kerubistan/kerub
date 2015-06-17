61d744c153b38c13ec19353df8d9b45994332904
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-06-17 17:55:45 +0200

    Update DaoOperations.kt

--- src/main/kotlin/com/github/K0zka/kerub/data/DaoOperations.kt
+++ src/main/kotlin/com/github/K0zka/kerub/data/DaoOperations.kt
@@ -2,20 +2,36 @@ package com.github.K0zka.kerub.data
 
 import com.github.K0zka.kerub.model.Entity
 
+/**
+ * Collection of common DAO operations to pick from when building DAO interfaces.
+ */
 public interface DaoOperations {
+
+	/**
+	 * Interface to allow adding entities to the backing data store.
+	 */
 	interface Add<T : Entity<I>, I> {
 		fun add(entity : T) : I
 	}
 
+	/**
+	 * Interface to allow removing entities from the backing data store.
+	 */
 	interface Remove<T : Entity<I>, I> {
 		fun remove(entity : T)
 		fun remove(id : I)
 	}
 
+	/**
+	 * Interface to allow replacing/updating entities on the backing store.
+	 */
 	interface Update<T : Entity<I>, I> {
 		fun update(entity : T)
 	}
 
+	/**
+	 * Interface to allow retrieving entities from the backing store.
+	 */
 	interface Read<T : Entity<I>, I> {
 		fun get(id : I) : T?
 	}
@@ -24,7 +40,13 @@ public interface DaoOperations {
 	 * List operations for DAOs.
 	 */
 	interface List<T : Entity<I>, I> {
+		/**
+		 * Get the total count of entities.
+		 */
 		fun count() : Int
+		/**
+		 * List the entities.
+		 */
 		fun list(
 				start : Long = 0,
 				limit : Long = java.lang.Long.MAX_VALUE,
