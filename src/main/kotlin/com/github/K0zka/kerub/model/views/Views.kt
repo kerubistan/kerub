5784cd43478d7c6eb5d711f5dbdc1fa9cc597fbe
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-06-17 08:59:23 +0200

    Update Views.kt

--- src/main/kotlin/com/github/K0zka/kerub/model/views/Views.kt
+++ src/main/kotlin/com/github/K0zka/kerub/model/views/Views.kt
@@ -1,5 +1,19 @@
 package com.github.K0zka.kerub.model.views
 
+/*
+ * View definitions for Jackson JSON serialization.
+ */
+
+/**
+ * Simple view should show only the most vital information on entities, e.g. id and name.
+ * Intended use is dropdowns, simple lists.
+ */
 public open class Simple
+/**
+ * Detailed view should show the most often used properties of entities beyond the Simple view.
+ */
 public open class Detailed : Simple()
+/**
+ * Full view should show all the properties of an entity.
+ */
 public open class Full : Detailed()
