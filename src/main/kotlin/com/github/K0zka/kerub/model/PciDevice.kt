ecbdcecf7ff789a2ebe898df5b9fc4699f17cd1d
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-06-17 14:31:01 +0200

    Update PciDevice.kt

--- src/main/kotlin/com/github/K0zka/kerub/model/PciDevice.kt
+++ src/main/kotlin/com/github/K0zka/kerub/model/PciDevice.kt
@@ -3,6 +3,9 @@ package com.github.K0zka.kerub.model
 import java.io.Serializable
 import org.hibernate.search.annotations.Field
 
+/**
+ * Represents a PCI device of the host.
+ */
 public class PciDevice(
 		Field
 		val address: String,
