6f91faf7b21d74f9ad2e2b840938e8ffdba1d1ea
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-08-09 18:58:43 +0200

    Added jackson annotations
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/SoftwarePackage.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/SoftwarePackage.kt
@@ -1,11 +1,14 @@
 package com.github.K0zka.kerub.utils
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.github.K0zka.kerub.model.Version
 import org.hibernate.search.annotations.Field
 import java.io.Serializable
 
 public data class SoftwarePackage(
+		JsonProperty("name")
 		Field val name: String,
+		JsonProperty("ver")
 		Field val version: Version) : Serializable {
 	override
 	fun toString(): String {
