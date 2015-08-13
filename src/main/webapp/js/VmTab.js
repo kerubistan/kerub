ad2910c5be173230d9692aad262dbc819afac96b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-08-13 07:18:15 +0200

    list vms
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/webapp/js/VmTab.js
+++ src/main/webapp/js/VmTab.js
@@ -1,5 +1,12 @@
 
 kerubApp.controller('VmTab', function($scope, $modal, $log, $socket, $appsession) {
+	$scope.vms = [];
+	$scope.refreshVms = function() {
+		$appsession.get('s/r/vm').success(function(result) {
+			$scope.vms = result.result;
+		});
+	};
+	$scope.refreshVms();
 	$scope.newVmForm = function () {
 		var modalInstance = $modal.open({
 		   templateUrl : 'NewVmWizard.html',
