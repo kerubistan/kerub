9e04157acd49884ea1631d49eae141d1fef607d5
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-08-19 22:31:41 +0200

    Exception mappers for resolution issues
    Bug-Url: https://github.com/K0zka/kerub/issues/40
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/main/kotlin/com/github/K0zka/kerub/exceptions/mappers/UnresolvedAddressExceptionMapper.kt
@@ -0,0 +1,11 @@
+package com.github.K0zka.kerub.exceptions.mappers
+
+import com.github.K0zka.kerub.security.mappers.RestError
+import java.nio.channels.UnresolvedAddressException
+import javax.ws.rs.core.Response
+import javax.ws.rs.ext.ExceptionMapper
+
+public class UnresolvedAddressExceptionMapper : ExceptionMapper<UnresolvedAddressException> {
+	override fun toResponse(exception: UnresolvedAddressException?): Response =
+			Response.status(Response.Status.NOT_ACCEPTABLE).entity(RestError("HOST2", "Address can not be resolved")).build()
+}
---
+++ src/test/kotlin/com/github/K0zka/kerub/exceptions/mappers/UnresolvedAddressExceptionMapperTest.kt
@@ -0,0 +1,15 @@
+package com.github.K0zka.kerub.exceptions.mappers
+
+import org.junit.Assert
+import org.junit.Test
+import java.nio.channels.UnresolvedAddressException
+import javax.ws.rs.core.Response
+
+public class UnresolvedAddressExceptionMapperTest {
+	Test
+	fun toResponse() {
+		val response = UnresolvedAddressExceptionMapper().toResponse(UnresolvedAddressException())
+		Assert.assertEquals(response.getStatus(), Response.Status.NOT_ACCEPTABLE.getStatusCode())
+		Assert.assertNotNull(response.getEntity())
+	}
+}
