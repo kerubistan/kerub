daf5138dded351d86550ef7450734c31f2e5c628
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-08-24 13:13:48 +0200

    Allow override motd from property file
    Bug-Url: https://github.com/K0zka/kerub/issues/70
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/services/impl/MotdServiceImpl.kt
+++ src/main/kotlin/com/github/K0zka/kerub/services/impl/MotdServiceImpl.kt
@@ -6,7 +6,7 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-public class MotdServiceImpl (val motd : String) : MotdService {
+public class MotdServiceImpl(var motd : String) : MotdService {
 	override
 	fun get() : String = motd
 }
