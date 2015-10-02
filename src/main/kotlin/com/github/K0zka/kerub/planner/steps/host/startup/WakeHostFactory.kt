761a008b98911958f364e280e5fa31e88a47b704
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-10-02 07:25:31 +0200

    WakeHost factory and test
    Bug-Url: https://github.com/K0zka/kerub/issues/23
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/planner/steps/host/startup/WakeHostFactory.kt
+++ src/main/kotlin/com/github/K0zka/kerub/planner/steps/host/startup/WakeHostFactory.kt
@@ -1,10 +1,17 @@
 package com.github.K0zka.kerub.planner.steps.host.startup
 
+import com.github.K0zka.kerub.model.dynamic.HostStatus
 import com.github.K0zka.kerub.planner.OperationalState
 import com.github.K0zka.kerub.planner.steps.AbstractOperationalStepFactory
 
+/**
+ * Generates steps to wake up non-running hosts.
+ */
 public object WakeHostFactory : AbstractOperationalStepFactory<WakeHost>() {
 	override fun produce(state: OperationalState): List<WakeHost> {
-		return listOf()
+		return state.hosts.filter {
+			val dyn = state.hostDyns[it.key]
+			dyn == null || dyn.status == HostStatus.Down
+		} .map { WakeHost(it.getValue()) }
 	}
 }
---
+++ src/test/kotlin/com/github/K0zka/kerub/planner/steps/host/startup/WakeHostFactoryTest.kt
@@ -0,0 +1,51 @@
+package com.github.K0zka.kerub.planner.steps.host.startup
+
+import com.github.K0zka.kerub.model.Host
+import com.github.K0zka.kerub.model.dynamic.HostDynamic
+import com.github.K0zka.kerub.model.dynamic.HostStatus
+import com.github.K0zka.kerub.planner.OperationalState
+import org.junit.Assert
+import org.junit.Test
+
+public class WakeHostFactoryTest {
+
+	val host1 = Host(
+			address = "host-1.example.com",
+	        dedicated = true,
+	        publicKey = "test"
+	                )
+
+	val host2 = Host(
+			address = "host-2.example.com",
+			dedicated = true,
+			publicKey = "test"
+	                )
+
+	@Test
+	fun produceSingleHostNorecord() {
+		val steps = WakeHostFactory.produce(OperationalState.fromLists(hosts = listOf(host1)))
+		Assert.assertEquals(steps.size(), 1)
+		Assert.assertTrue(steps.all { it is WakeHost && it.host == host1 })
+	}
+
+	@Test
+	fun produceSingleHostDown() {
+		val steps = WakeHostFactory.produce(OperationalState.fromLists(
+				hosts = listOf(host1),
+		        hostDyns = listOf(HostDynamic(id = host1.id, status = HostStatus.Down))
+				))
+		Assert.assertEquals(steps.size(), 1)
+		Assert.assertTrue(steps.all { it is WakeHost && it.host == host1 })
+	}
+
+	@Test
+	fun produceOneUpOneDown() {
+		val steps = WakeHostFactory.produce(OperationalState.fromLists(
+				hosts = listOf(host1, host2),
+				hostDyns = listOf(HostDynamic(id = host2.id, status = HostStatus.Up))
+		                                                              ))
+		Assert.assertEquals(steps.size(), 1)
+		Assert.assertTrue(steps.all { it is WakeHost && it.host == host1 })
+	}
+
+}
