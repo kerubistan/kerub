ded691010246a750e9d9d694da4e32b2a15b5259
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-11-04 07:07:08 +0100

    Added MPStat to monitoring processes, some refactoring to reduce duplication
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/host/distros/AbstractLinux.kt
+++ src/main/kotlin/com/github/K0zka/kerub/host/distros/AbstractLinux.kt
@@ -5,8 +5,10 @@ import com.github.K0zka.kerub.model.Host
 import com.github.K0zka.kerub.model.dynamic.HostDynamic
 import com.github.K0zka.kerub.model.dynamic.HostStatus
 import com.github.K0zka.kerub.utils.getLogger
+import com.github.K0zka.kerub.utils.junix.mpstat.MPStat
 import com.github.K0zka.kerub.utils.junix.vmstat.VmStat
 import org.apache.sshd.ClientSession
+import java.util.UUID
 
 public abstract class AbstractLinux : Distribution {
 
@@ -14,30 +16,33 @@ public abstract class AbstractLinux : Distribution {
 		val logger = getLogger(AbstractLinux::class)
 	}
 
+	fun doWithDyn(id: UUID, hostDynDao: HostDynamicDao, action: (HostDynamic) -> HostDynamic) {
+		val hostDyn = hostDynDao.get(id)
+		if (hostDyn == null) {
+			val newHostDyn = HostDynamic(
+					id = id,
+					status = HostStatus.Up
+			)
+			hostDynDao.add(action(newHostDyn))
+		} else {
+			hostDynDao.update(action(hostDyn))
+		}
+	}
+
 	override fun startMonitorProcesses(session: ClientSession, host: Host, hostDynDao: HostDynamicDao) {
 		val id = host.id
+		MPStat.monitor(session, {
+			doWithDyn(id, hostDynDao, { it /*TODO*/ })
+		})
 		VmStat.vmstat(session, { event ->
-			val hostDyn = hostDynDao.get(id)
-			logger.debug("{} vmstat : {}\t{}\t{}", id, event.userCpu, event.systemCpu, event.idleCpu)
-			if (hostDyn == null) {
-				val newHostDyn = HostDynamic(
-						id = id,
+			doWithDyn(id, hostDynDao, {
+				it.copy(
 						status = HostStatus.Up,
 						idleCpu = event.idleCpu,
 						systemCpu = event.systemCpu,
 						userCpu = event.userCpu
-				                            )
-				hostDynDao.add(newHostDyn)
-			} else {
-				val newHostDyn = hostDyn.copy(
-						status = HostStatus.Up,
-						idleCpu = event.idleCpu,
-						systemCpu = event.systemCpu,
-						userCpu = event.userCpu
-				                             )
-				hostDynDao.update(newHostDyn)
-			}
-
+				)
+			})
 		})
 	}
 }
