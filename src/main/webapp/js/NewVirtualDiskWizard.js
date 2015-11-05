5a1d456d809849a1b54cdd6199c248dcc4158298
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-11-05 16:34:31 +0100

    human-friendly size display in new virtual disk dialog
    Bug-Url: https://github.com/kerubistan/kerub/issues/82
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/webapp/js/NewVirtualDiskWizard.js
+++ src/main/webapp/js/NewVirtualDiskWizard.js
@@ -1,12 +1,30 @@
 var NewVirtualDiskWizard = function($scope, $modalInstance, $log, appsession, uuid4, size) {
 	var id = uuid4.generate();
+
 	$scope.disk = {
 		'@type' : 'vstorage',
 		id : id,
 		name : 'disk-'+id,
-		size : 16 * 1024 * 1024 * 1024,
+		size : size.toSize('16 GB'),
+		userFriendlySize : function(newSize) {
+			$log.debug(newSize);
+			if(newSize) {
+				$scope.disk.size = size.toSize(newSize);
+			}
+			return size.humanFriendlySize($scope.disk.size);
+		},
 		expectations : []
 	};
+
+	/**
+	 * gettersetter for disk size
+	 */
+	$scope.diskSize = function(val) {
+		if(val) {
+			$log.info(val);
+		} else {
+		}
+	}
 
 	$scope.addStorage = function() {
 		appsession.put('s/r/virtual-storage', $scope.disk).success(function(result) {
--- src/main/webapp/js/SizeUtil.js
+++ src/main/webapp/js/SizeUtil.js
@@ -1,17 +1,31 @@
 kerubApp.factory('size', ['$log', function($log) {
+	var mul = 1024;
+	var kb = mul;
+	var mb = mul * kb;
+	var gb = mul * mb;
+	var tb = mul * gb;
+	var pb = mul * tb;
 	return {
 
         toSize: function (sizeStr) {
-
+            clean = sizeStr.toLowerCase().trim();
+            var amount = parseFloat(clean);
+            if(clean.endsWith('kb')) {
+				return amount * kb;
+            } else if(clean.endsWith('mb')) {
+                return amount * mb;
+            } else if(clean.endsWith('gb')) {
+                return amount * gb;
+            } else if(clean.endsWith('tb')) {
+                return amount * tb;
+            } else if(clean.endsWith('pb')) {
+                return amount * pb;
+            } else {
+            	return amount;
+            }
         },
 
         humanFriendlySize : function(size) {
-        	var mul = 1024;
-        	var kb = mul;
-        	var mb = mul * kb;
-        	var gb = mul * mb;
-        	var tb = mul * gb;
-        	var pb = mul * tb;
         	if(size < kb) {
         		return size + ' B';
         	} else if(size < mb) {
