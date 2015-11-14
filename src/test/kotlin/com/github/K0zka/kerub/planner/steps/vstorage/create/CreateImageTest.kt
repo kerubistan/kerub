736e69497f0b12fa81832183ea50839694f3e9fb
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-11-14 22:07:26 +0100

    Test for the CreateImage state transformation
    Bug-Url: https://github.com/kerubistan/kerub/issues/23
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/test/kotlin/com/github/K0zka/kerub/planner/steps/vstorage/create/CreateImageTest.kt
@@ -0,0 +1,36 @@
+package com.github.K0zka.kerub.planner.steps.vstorage.create
+
+import com.github.K0zka.kerub.model.Host
+import com.github.K0zka.kerub.model.VirtualStorageDevice
+import com.github.K0zka.kerub.planner.OperationalState
+import com.github.K0zka.kerub.utils.toSize
+import org.junit.Assert
+import org.junit.Test
+
+class CreateImageTest {
+
+	val device = VirtualStorageDevice(
+			size = "128 GB".toSize(),
+			readOnly = false,
+			name = "test"
+	)
+
+	val host = Host(
+			address = "host-1.example.com",
+			publicKey = "",
+			dedicated = true
+	)
+
+	@Test
+	fun take() {
+		val state = CreateImage(device, host).take(OperationalState.fromLists(
+				hosts = listOf(host),
+				vStorage = listOf(device)
+		))
+
+		Assert.assertTrue(state.vStorageDyns.values.any {
+			it.hostId == host.id
+					&& it.id == device.id
+		})
+	}
+}
