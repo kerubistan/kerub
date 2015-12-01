fb4d458a2640df388dc55d4447d12f320d34594e
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-12-01 21:00:10 +0100

    cleanup: removed generated comment
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/JmsEventListener.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/JmsEventListener.kt
@@ -4,9 +4,6 @@ import com.github.K0zka.kerub.data.EventListener
 import com.github.K0zka.kerub.model.messages.Message
 import org.springframework.jms.core.JmsTemplate
 
-/**
- * Created by kocka on 10/10/14.
- */
 class JmsEventListener(val template : JmsTemplate) : EventListener {
     override fun send(message: Message) {
         template.send( { it?.createObjectMessage(message) } )
