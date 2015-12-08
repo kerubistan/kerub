e01f9994078dfc7ab180c070c070da0a7a70b59b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-12-08 19:29:07 +0100

    added logger initializer with string param
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/logUtils.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/logUtils.kt
@@ -7,3 +7,7 @@ import kotlin.reflect.KClass
 fun getLogger(cl : KClass<*>) : Logger {
 	return LoggerFactory.getLogger(cl.java)!!
 }
+
+fun getLogger(logger : String) : Logger {
+	return LoggerFactory.getLogger(logger)!!
+}
