08a7faade12bd3b40c4ef1d47c312745b790c9ee
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2015-12-21 13:13:31 +0100

    fix: CpuStat needs to be serializable
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/model/dynamic/CpuStat.kt
+++ src/main/kotlin/com/github/K0zka/kerub/model/dynamic/CpuStat.kt
@@ -1,12 +1,14 @@
 package com.github.K0zka.kerub.model.dynamic
 
+import java.io.Serializable
+
 data class CpuStat(
 		val cpuNr: Int,
 		val user: Float,
 		val system: Float,
 		val ioWait: Float,
 		val idle: Float
-) {
+) : Serializable {
 	companion object {
 		val zero = CpuStat(0, 0f, 0f, 0f, 0f)
 	}
