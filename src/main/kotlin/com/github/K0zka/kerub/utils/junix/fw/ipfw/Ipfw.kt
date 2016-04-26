489c25226f4b081165a146eea2eac1f2f3cc9064
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-04-26 07:42:03 +0200

    stub for ipfw junix wrapper
    Bug-Url: https://github.com/kerubistan/kerub/issues/142
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/main/kotlin/com/github/K0zka/kerub/utils/junix/fw/ipfw/Ipfw.kt
@@ -0,0 +1,12 @@
+package com.github.K0zka.kerub.utils.junix.fw.ipfw
+
+import org.apache.sshd.client.session.ClientSession
+
+object Ipfw {
+	fun open(session : ClientSession, port : Int, proto : String) {
+		TODO("https://github.com/kerubistan/kerub/issues/142")
+	}
+	fun close(session : ClientSession, port : Int, proto : String) {
+		TODO("https://github.com/kerubistan/kerub/issues/142")
+	}
+}
