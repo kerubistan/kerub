a279cf07946df13d75b6a926c6dd29b33e3e0824
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-04-28 23:04:30 +0200

    Implementation and test for iptables firewall adapter
    Bug-Url: https://github.com/kerubistan/kerub/issues/142
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/host/fw/IptablesFireWall.kt
+++ src/main/kotlin/com/github/K0zka/kerub/host/fw/IptablesFireWall.kt
@@ -1,14 +1,15 @@
 package com.github.K0zka.kerub.host.fw
 
 import com.github.K0zka.kerub.host.FireWall
+import com.github.K0zka.kerub.utils.junix.fw.iptables.IpTables
 import org.apache.sshd.client.session.ClientSession
 
 class IptablesFireWall(private val session : ClientSession) : FireWall {
 	override fun open(port: Int, proto: String) {
-		throw UnsupportedOperationException()
+		IpTables.open(session, port, proto)
 	}
 
 	override fun close(port: Int, proto: String) {
-		throw UnsupportedOperationException()
+		IpTables.close(session, port, proto)
 	}
 }
