66a4e1ce7315b78a4a50ae310b57a87fa254dc7a
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-05-09 07:33:56 +0200

    disk data transfer: use scp client instead of sftp
    when uploading large files with sftp the channel was closed unexpectedly
    
    Bug-Url: https://github.com/kerubistan/kerub/issues/143
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/services/impl/VirtualStorageDeviceServiceImpl.kt
+++ src/main/kotlin/com/github/K0zka/kerub/services/impl/VirtualStorageDeviceServiceImpl.kt
@@ -4,35 +4,35 @@ import com.github.K0zka.kerub.data.HostDao
 import com.github.K0zka.kerub.data.VirtualStorageDeviceDao
 import com.github.K0zka.kerub.data.dynamic.VirtualStorageDeviceDynamicDao
 import com.github.K0zka.kerub.host.HostCommandExecutor
-import com.github.K0zka.kerub.host.use
 import com.github.K0zka.kerub.model.VirtualStorageDevice
 import com.github.K0zka.kerub.model.dynamic.VirtualStorageDeviceDynamic
-import com.github.K0zka.kerub.model.dynamic.VirtualStorageFsAllocation
 import com.github.K0zka.kerub.model.dynamic.VirtualStorageLvmAllocation
 import com.github.K0zka.kerub.model.expectations.StorageAvailabilityExpectation
 import com.github.K0zka.kerub.model.paging.SearchResultPage
 import com.github.K0zka.kerub.services.VirtualStorageDeviceService
 import org.apache.sshd.client.session.ClientSession
+import org.apache.sshd.common.scp.ScpTimestamp
 import java.io.InputStream
+import java.nio.file.attribute.PosixFilePermission
 import java.util.UUID
 import javax.ws.rs.container.AsyncResponse
 
 class VirtualStorageDeviceServiceImpl(
 		dao: VirtualStorageDeviceDao,
-		private val dynDao : VirtualStorageDeviceDynamicDao,
-		private val hostDao : HostDao,
+		private val dynDao: VirtualStorageDeviceDynamicDao,
+		private val hostDao: HostDao,
 		private val executor: HostCommandExecutor
 ) : VirtualStorageDeviceService,
 		ListableBaseService<VirtualStorageDevice>(dao, "virtual disk") {
 
-	override fun load(id: UUID, async : AsyncResponse, data: InputStream) {
+	override fun load(id: UUID, async: AsyncResponse, data: InputStream) {
 		val device = getById(id)
 		dynDao.waitFor(id) {
 			dyn ->
 			val host = requireNotNull(hostDao[dyn.allocation.hostId])
 			executor.dataConnection(host, {
 				session ->
-				pump(data, dyn, session)
+				pump(data, device, dyn, session)
 
 				dao.update(device.copy(
 						expectations = device.expectations.filterNot { it is StorageAvailabilityExpectation }
@@ -48,19 +48,19 @@ class VirtualStorageDeviceServiceImpl(
 
 	}
 
-	private fun pump(data: InputStream, dyn: VirtualStorageDeviceDynamic, session: ClientSession) {
-		session.createSftpClient().use {
-			sftp ->
-			when (dyn.allocation) {
-				is VirtualStorageLvmAllocation -> {
-					sftp.write(dyn.allocation.path).use {
-						dev ->
-						data.copyTo(dev)
-					}
-				}
-				else -> {
-					TODO()
-				}
+	private fun pump(data: InputStream, device: VirtualStorageDevice, dyn: VirtualStorageDeviceDynamic, session: ClientSession) {
+		when (dyn.allocation) {
+			is VirtualStorageLvmAllocation -> {
+				session.createScpClient().upload(
+						data,
+						dyn.allocation.path,
+						device.size.toLong(),
+						listOf(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE),
+						ScpTimestamp(System.currentTimeMillis(), System.currentTimeMillis())
+				)
+			}
+			else -> {
+				TODO()
 			}
 		}
 	}
