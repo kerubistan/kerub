d01a3a50dd6a392ed141f9a21eb843ba7a85ac59
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-07-24 11:53:24 +0200

    cygwin utility to list packages
    Bug-Url: https://github.com/kerubistan/kerub/issues/94
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/test/kotlin/com/github/K0zka/kerub/utils/junix/packagemanager/cygwin/CygCheckTest.kt
@@ -0,0 +1,35 @@
+package com.github.K0zka.kerub.utils.junix.packagemanager.cygwin
+
+import com.github.K0zka.kerub.utils.resource
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.apache.commons.io.input.NullInputStream
+import org.apache.sshd.client.channel.ChannelExec
+import org.apache.sshd.client.future.OpenFuture
+import org.apache.sshd.client.session.ClientSession
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CygCheckTest {
+
+	val session: ClientSession = mock()
+	val execChannel: ChannelExec = mock()
+	val openFuture: OpenFuture = mock()
+
+	@Test
+	fun testListPackages() {
+		whenever(session.createExecChannel(any())).thenReturn(execChannel)
+		whenever(execChannel.open()).thenReturn(openFuture)
+		whenever(execChannel.invertedErr).thenReturn(NullInputStream(0))
+		whenever(execChannel.invertedOut).then {
+			resource("com/github/K0zka/kerub/utils/junix/cygwin/cygcheck-d-c.txt")
+		}
+
+		val packages = CygCheck.listPackages(session)
+
+		assertFalse(packages.isEmpty())
+		assertTrue(packages.any { it.name == "sed" && it.version.major == "4" && it.version.minor == "2" })
+	}
+}
