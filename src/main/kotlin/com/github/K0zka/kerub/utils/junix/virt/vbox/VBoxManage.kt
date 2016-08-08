43d083dc50e4326a1673a40bf6adb104552cc20c
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-08-08 21:29:03 +0200

    virtualbox xml format removed (not supported by developers) replaced by vboxmanage commands
    Bug-Url: https://github.com/kerubistan/kerub/issues/93
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/junix/virt/vbox/VBoxManage.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/junix/virt/vbox/VBoxManage.kt
@@ -1,102 +1,72 @@
 package com.github.K0zka.kerub.utils.junix.virt.vbox
 
 import com.github.K0zka.kerub.host.executeOrDie
+import com.github.K0zka.kerub.model.Host
 import com.github.K0zka.kerub.model.VirtualMachine
+import com.github.K0zka.kerub.model.VirtualStorageDevice
+import com.github.K0zka.kerub.model.dynamic.VirtualStorageDeviceDynamic
+import com.github.K0zka.kerub.model.dynamic.VirtualStorageFsAllocation
+import com.github.K0zka.kerub.model.dynamic.VirtualStorageGvinumAllocation
+import com.github.K0zka.kerub.model.dynamic.VirtualStorageLvmAllocation
 import com.github.K0zka.kerub.model.io.DeviceType
 import com.github.K0zka.kerub.model.io.VirtualDiskFormat
+import com.github.K0zka.kerub.utils.MB
+import com.github.K0zka.kerub.utils.storage.iscsiStorageId
 import org.apache.sshd.client.session.ClientSession
+import java.math.BigDecimal
 import java.math.BigInteger
+import java.math.MathContext
+import java.math.RoundingMode
+import java.util.UUID
 
 object VBoxManage {
-	fun startVm(session: ClientSession, vm : VirtualMachine) {
-		val vmDescriptor = """
-<?xml version="1.0"?>
-<VirtualBox xmlns="http://www.innotek.de/VirtualBox-settings" version="1.14-freebsd">
-  <Machine uuid="{${vm.id}}" name="${vm.id}" OSType="Other" snapshotFolder="Snapshots" lastStateChange="2016-07-31T15:14:04Z">
-    <MediaRegistry>
-      <HardDisks/>
-      <DVDImages/>
-      <FloppyImages/>
-    </MediaRegistry>
-    <Hardware version="2">
-      <CPU count="1" hotplug="false">
-        <HardwareVirtEx enabled="true"/>
-        <HardwareVirtExNestedPaging enabled="true"/>
-        <HardwareVirtExVPID enabled="true"/>
-        <HardwareVirtExUX enabled="true"/>
-        <PAE enabled="true"/>
-        <LongMode enabled="true"/>
-        <HardwareVirtExLargePages enabled="true"/>
-        <HardwareVirtForce enabled="false"/>
-      </CPU>
-      <Memory RAMSize="128" PageFusion="false"/>
-      <HID Pointing="PS2Mouse" Keyboard="PS2Keyboard"/>
-      <HPET enabled="false"/>
-      <Chipset type="PIIX3"/>
-      <Boot>
-        <Order position="1" device="DVD"/>
-        <Order position="2" device="HardDisk"/>
-        <Order position="3" device="None"/>
-      </Boot>
-      <Display VRAMSize="8" monitorCount="1" accelerate3D="false" accelerate2DVideo="false"/>
-      <VideoCapture enabled="false" screens="18446744073709551615" horzRes="1024" vertRes="768" rate="512" fps="25"/>
-      <RemoteDisplay enabled="false" authType="Null"/>
-      <BIOS>
-        <ACPI enabled="true"/>
-        <IOAPIC enabled="false"/>
-        <Logo fadeIn="true" fadeOut="true" displayTime="0"/>
-        <BootMenu mode="MessageAndMenu"/>
-        <TimeOffset value="0"/>
-        <PXEDebug enabled="false"/>
-      </BIOS>
-      <USB>
-        <Controllers/>
-        <DeviceFilters/>
-      </USB>
-      <Network>
-      </Network>
-      <UART>
-        <Port slot="0" enabled="false" IOBase="0x3f8" IRQ="4" hostMode="Disconnected"/>
-        <Port slot="1" enabled="false" IOBase="0x3f8" IRQ="4" hostMode="Disconnected"/>
-      </UART>
-      <LPT>
-        <Port slot="0" enabled="false" IOBase="0x378" IRQ="7"/>
-        <Port slot="1" enabled="false" IOBase="0x378" IRQ="7"/>
-      </LPT>
-      <AudioAdapter controller="AC97" driver="OSS" enabled="false"/>
-      <RTC localOrUTC="local"/>
-      <SharedFolders/>
-      <Clipboard mode="Disabled"/>
-      <DragAndDrop mode="Disabled"/>
-      <IO>
-        <IoCache enabled="true" size="5"/>
-        <BandwidthGroups/>
-      </IO>
-      <HostPci>
-        <Devices/>
-      </HostPci>
-      <EmulatedUSB>
-        <CardReader enabled="false"/>
-      </EmulatedUSB>
-      <Guest memoryBalloonSize="0"/>
-      <GuestProperties/>
-    </Hardware>
-    <StorageControllers/>
-  </Machine>
-</VirtualBox>
-		"""
-		session.createSftpClient().use {
-			sftp ->
-			sftp.write("/tmp/${vm.id}.xml").writer(charset("ASCII")).use {
-				it.write(vmDescriptor)
-			}
+
+	private val mc = MathContext(1, RoundingMode.UP)
+	private val mediatype = mapOf(
+			DeviceType.disk to "hdd",
+			DeviceType.cdrom to "dvddrive",
+			DeviceType.floppy to "floppy"
+	)
+
+	fun startVm(session: ClientSession, vm : VirtualMachine, targetHost: Host, storageMap : Map<UUID, Triple<VirtualStorageDevice, VirtualStorageDeviceDynamic, Host>>) {
+		session.executeOrDie("VBoxManage createvm --name ${vm.id} --uuid ${vm.id} --register")
+		// memory is meant in megabytes, kerub has it in bytes, let's round it up
+		session.executeOrDie("VBoxManage modifyvm --memory ${(BigDecimal(vm.memory.min) / MB).round(mc)} ")
+		session.executeOrDie("VBoxManage modifyvm --cpus ${vm.nrOfCpus} ")
+		vm.virtualStorageLinks.forEach {
+			link ->
+			val storage = requireNotNull(storageMap[link.virtualStorageId])
+			val device = storage.first
+			val dyn = storage.second
+			val host = storage.third
+			session.executeOrDie(
+					"VBoxManage storageattach ${vm.id} " +
+							"--storagectl ${link.virtualStorageId} " +
+							"--type ${mediatype[link.device]}" +
+							"--medium ${medium(dyn, host, targetHost)}"
+			)
 		}
-		session.executeOrDie("VBoxManage registervm /tmp/${vm.id}.xml")
 		session.executeOrDie("VBoxManage start ${vm.id}")
-		TODO("https://github.com/kerubistan/kerub/issues/93")
 	}
-	fun stopVm(session: ClientSession) {
-		TODO("https://github.com/kerubistan/kerub/issues/93")
+
+	private fun medium(dyn: VirtualStorageDeviceDynamic, storageHost: Host, targetHost : Host): String {
+		if(targetHost.id == storageHost.id) {
+			val allocation = dyn.allocation
+			return when(allocation) {
+				is VirtualStorageLvmAllocation -> { allocation.path }
+				is VirtualStorageGvinumAllocation -> { "/dev/gvinum/${dyn.id}" }
+				is VirtualStorageFsAllocation -> { "${allocation.mountPoint}/${dyn.id}" }
+				else -> {
+					TODO("Unknown allocation type: ${allocation}")
+				}
+			}
+		} else {
+			return "iscsi --server ${storageHost.address} --target ${iscsiStorageId(dyn.id) }"
+		}
+	}
+
+	fun stopVm(session: ClientSession, vm : VirtualMachine) {
+		session.executeOrDie("VBoxManage controlvm ${vm.id} poweroff")
 	}
 	fun createMedium(session: ClientSession, path : String, size : BigInteger, type: DeviceType, format: VirtualDiskFormat) {
 		session.executeOrDie("VBoxManage create $type --filename $path --format $format")
