5306c9b1a79eab87f86846547f9ce178fab01ad8
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-08-10 23:10:13 +0200

    available host lit moved to start-vm baseclass
    Bug-Url: https://github.com/kerubistan/kerub/issues/93
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/planner/steps/vm/start/AbstractStartVmFactory.kt
+++ src/main/kotlin/com/github/K0zka/kerub/planner/steps/vm/start/AbstractStartVmFactory.kt
@@ -1,13 +1,27 @@
 package com.github.K0zka.kerub.planner.steps.vm.start
 
+import com.github.K0zka.kerub.model.Host
 import com.github.K0zka.kerub.model.VirtualMachine
 import com.github.K0zka.kerub.model.VirtualMachineStatus
+import com.github.K0zka.kerub.model.dynamic.HostDynamic
+import com.github.K0zka.kerub.model.dynamic.HostStatus
 import com.github.K0zka.kerub.model.expectations.VirtualMachineAvailabilityExpectation
 import com.github.K0zka.kerub.planner.OperationalState
 import com.github.K0zka.kerub.planner.steps.AbstractOperationalStep
 import com.github.K0zka.kerub.planner.steps.AbstractOperationalStepFactory
+import com.github.K0zka.kerub.utils.toMap
 
 abstract class AbstractStartVmFactory<S : AbstractOperationalStep> : AbstractOperationalStepFactory<S>() {
+
+	fun getWorkingHosts(state: OperationalState) : Map<Host, HostDynamic> {
+		return state.hosts.values.map {
+			host ->
+			val dyn = state.hostDyns[host.id]
+			if(dyn != null && dyn.status == HostStatus.Up) host to dyn else null
+		}.filterNotNull().toMap()
+
+	}
+
 	fun getVmsToStart(state: OperationalState): List<VirtualMachine> {
 		val vmsToRun = state.vms.values.filter {
 			it.expectations.any {
--- src/main/kotlin/com/github/K0zka/kerub/planner/steps/vm/start/kvm/KvmStartVirtualMachineFactory.kt
+++ src/main/kotlin/com/github/K0zka/kerub/planner/steps/vm/start/kvm/KvmStartVirtualMachineFactory.kt
@@ -4,28 +4,16 @@ import com.github.K0zka.kerub.planner.OperationalState
 import com.github.K0zka.kerub.planner.steps.vm.match
 import com.github.K0zka.kerub.planner.steps.vm.start.AbstractStartVmFactory
 import com.github.K0zka.kerub.planner.steps.vm.storageAllocationMap
+import com.github.K0zka.kerub.utils.join
 
 object KvmStartVirtualMachineFactory : AbstractStartVmFactory<KvmStartVirtualMachine>() {
 
-	override fun produce(state: OperationalState): List<KvmStartVirtualMachine> {
-		val vmsToStart = getVmsToStart(state)
-
-		var steps = listOf<KvmStartVirtualMachine>()
-
-		vmsToStart.forEach {
-			vm ->
-			val vstorageState = storageAllocationMap(state)
-			state.hosts.values.forEach {
-				host ->
-				val dyn = state.hostDyns[host.id]
-				if (match(host, dyn, vm, vstorageState)) {
-					steps += KvmStartVirtualMachine(vm, host)
+	override fun produce(state: OperationalState): List<KvmStartVirtualMachine> =
+			getVmsToStart(state).map {
+				vm ->
+				getWorkingHosts(state).filter { match(it.key, it.value, vm, storageAllocationMap(state)) }.map {
+					KvmStartVirtualMachine(vm, it.key)
 				}
-			}
-		}
-
-		return steps
-	}
-
+			}.join()
 
 }
