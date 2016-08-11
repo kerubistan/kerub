72a58d0b607f4f6c7164f1612e4fb64a9aa89442
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-08-11 17:54:50 +0200

    pkg fix  - use lines() instead of the split()  - filter out empty line - it causes the parser to fail
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/junix/packagemanager/pkg/Pkg.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/junix/packagemanager/pkg/Pkg.kt
@@ -3,9 +3,12 @@ package com.github.K0zka.kerub.utils.junix.packagemanager.pkg
 import com.github.K0zka.kerub.host.execute
 import com.github.K0zka.kerub.model.SoftwarePackage
 import com.github.K0zka.kerub.model.Version
+import com.github.K0zka.kerub.utils.getLogger
 import org.apache.sshd.client.session.ClientSession
 
 object Pkg {
+
+	val logger = getLogger(Pkg::class)
 
 	fun installPackage(session: ClientSession, vararg packs: String) {
 		require(packs.isNotEmpty())
@@ -18,7 +21,7 @@ object Pkg {
 	}
 
 	fun listPackages(session: ClientSession): List<SoftwarePackage>
-			= session.execute("""pkg query "%n\t%v" """).split('\n').toList().map {
+			= session.execute("""pkg query "%n\t%v" """).lines().filter { it.isNotEmpty() }.map {
 		val split = it.split('\t')
 		SoftwarePackage(split[0], Version.fromVersionString(split[1]))
 	}
