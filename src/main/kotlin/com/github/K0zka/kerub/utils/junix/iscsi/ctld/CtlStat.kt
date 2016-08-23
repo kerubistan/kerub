dc0d2e4a86d9312b18559413ced272e584eb7b65
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-08-23 17:30:18 +0200

    some (incomplete) ctld bits
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/main/kotlin/com/github/K0zka/kerub/utils/junix/iscsi/ctld/CtlStat.kt
@@ -0,0 +1,7 @@
+package com.github.K0zka.kerub.utils.junix.iscsi.ctld
+
+import com.github.K0zka.kerub.utils.junix.common.OsCommand
+
+object CtlStat : OsCommand {
+
+}
--- src/main/kotlin/com/github/K0zka/kerub/utils/junix/iscsi/ctld/Ctld.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/junix/iscsi/ctld/Ctld.kt
@@ -1,12 +1,39 @@
 package com.github.K0zka.kerub.utils.junix.iscsi.ctld
 
 import com.github.K0zka.kerub.utils.junix.common.OsCommand
+import com.github.K0zka.kerub.utils.storage.iscsiStorageId
 import org.apache.sshd.client.session.ClientSession
 import java.util.UUID
 
 object Ctld : OsCommand {
 	fun share(session: ClientSession, id: UUID, path: String, readOnly: Boolean = false) {
+		val config = """
+	#begin ${id}
+
+     auth-group ag-${id} {
+     	#TODO
+     	auth-type none
+     }
+
+	target ${iscsiStorageId(id)} {
+	auth-group ag-${id}
+	portal-group kerub
+
+		lun 0 {
+			path $path
+			${readonlyOption(readOnly)}
+		}
+	}
+	#end ${id}
+
+		"""
 		TODO()
+	}
+
+	private fun readonlyOption(readOnly: Boolean) = if (readOnly) {
+		"readonly on"
+	} else {
+		""
 	}
 
 	fun unshare(session: ClientSession) {
