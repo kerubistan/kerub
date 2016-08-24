679450f077d449972a5352907fe9e369736c9374
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-08-24 06:36:22 +0200

    storage management detection overridden in UbuntuBSD
    Bug-Url: https://github.com/kerubistan/kerub/issues/170
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/host/distros/UbuntuBSD.kt
+++ src/main/kotlin/com/github/K0zka/kerub/host/distros/UbuntuBSD.kt
@@ -2,8 +2,11 @@ package com.github.K0zka.kerub.host.distros
 
 import com.github.K0zka.kerub.host.PackageManager
 import com.github.K0zka.kerub.host.packman.RaspbianPackageManager
+import com.github.K0zka.kerub.model.FsStorageCapability
+import com.github.K0zka.kerub.model.StorageCapability
 import com.github.K0zka.kerub.model.Version
 import com.github.K0zka.kerub.model.lom.PowerManagementInfo
+import com.github.K0zka.kerub.utils.junix.df.DF
 import com.github.K0zka.kerub.utils.silent
 import org.apache.sshd.client.session.ClientSession
 
@@ -17,5 +20,15 @@ class UbuntuBSD : AbstractDebian("ubuntuBSD") {
 	//the output format of dpkg is also not conventional ubuntu
 	override fun getPackageManager(session: ClientSession): PackageManager
 			= RaspbianPackageManager(session)
+
+	//only ZFS "volume manager" in ubuntuBSD https://github.com/kerubistan/kerub/issues/174
+	override fun detectStorageCapabilities(session: ClientSession): List<StorageCapability> =
+			DF.df(session).map {
+				mount ->
+				FsStorageCapability(
+						size = mount.free + mount.used,
+						mountPoint = mount.mountPoint
+				)
+			}
 
 }
