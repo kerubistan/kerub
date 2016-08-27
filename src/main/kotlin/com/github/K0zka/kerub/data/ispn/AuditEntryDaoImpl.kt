41e1010905e0d919668a76b9957f215006879614
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-08-27 19:37:15 +0200

    fix: audot dao list all 3 types of audit events
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/data/ispn/AuditEntryDaoImpl.kt
+++ src/main/kotlin/com/github/K0zka/kerub/data/ispn/AuditEntryDaoImpl.kt
@@ -1,24 +1,34 @@
 package com.github.K0zka.kerub.data.ispn
 
 import com.github.K0zka.kerub.data.AuditEntryDao
+import com.github.K0zka.kerub.model.AddEntry
 import com.github.K0zka.kerub.model.AuditEntry
+import com.github.K0zka.kerub.model.DeleteEntry
 import com.github.K0zka.kerub.model.UpdateEntry
 import org.infinispan.AdvancedCache
 import org.infinispan.query.Search
 import org.infinispan.query.dsl.Query
+import org.infinispan.query.dsl.QueryBuilder
 import java.util.UUID
+import kotlin.reflect.KClass
 
 class AuditEntryDaoImpl(protected val cache: AdvancedCache<UUID, AuditEntry>) : AuditEntryDao {
-	override fun add(entry: AuditEntry): UUID {
-		cache.putAsync(entry.id, entry)
-		return entry.id
+	override fun add(entity: AuditEntry): UUID {
+		cache.putAsync(entity.id, entity)
+		return entity.id
 	}
 
-	override fun listById(id: UUID): List<AuditEntry> {
-		return Search.getQueryFactory(cache)!!
-				.from(UpdateEntry::class.java)!!
-				.having("idStr").eq(id.toString()).toBuilder<Query>()
-				.build()!!
-				.list<AuditEntry>() as List<AuditEntry>
-	}
+	override fun listById(id: UUID): List<AuditEntry> =
+			criteria(builder(AddEntry::class), id) +
+					criteria(builder(UpdateEntry::class), id) +
+					criteria(builder(DeleteEntry::class), id)
+
+	private fun builder(clazz: KClass<*>) =
+			Search.getQueryFactory(cache).from(clazz.java)
+
+	private fun criteria(builder: QueryBuilder<*>, id: UUID): List<AuditEntry> =
+			builder
+					.having("idStr").eq(id.toString()).toBuilder<Query>()
+					.build()!!
+					.list<AuditEntry>()
 }
--- src/test/kotlin/com/github/K0zka/kerub/data/ispn/AuditEntryDaoImplTest.kt
+++ src/test/kotlin/com/github/K0zka/kerub/data/ispn/AuditEntryDaoImplTest.kt
@@ -1,7 +1,9 @@
 package com.github.K0zka.kerub.data.ispn
 
 import com.github.K0zka.kerub.data.AuditEntryDao
+import com.github.K0zka.kerub.model.AddEntry
 import com.github.K0zka.kerub.model.AuditEntry
+import com.github.K0zka.kerub.model.DeleteEntry
 import com.github.K0zka.kerub.model.Entity
 import com.github.K0zka.kerub.model.UpdateEntry
 import com.github.K0zka.kerub.utils.toUUID
@@ -43,17 +45,27 @@ import java.util.UUID
 
 	@Test
 	fun add() {
+
 		Mockito.`when`(oldEntity!!.id)!!
 				.thenReturn("43dcc6e7-cfcd-44af-a4e5-bbe8f7d948cc".toUUID())
 		Mockito.`when`(newEntity!!.id)!!
 				.thenReturn("43dcc6e7-cfcd-44af-a4e5-bbe8f7d948cc".toUUID())
+
+		dao!!.add(AddEntry(
+				new = oldEntity!!,
+				user = UUID.randomUUID().toString()))
+
 		dao!!.add(UpdateEntry(
 				old = oldEntity!!,
 				new = newEntity!!,
 				user = UUID.randomUUID().toString()))
 
+		dao!!.add(DeleteEntry(
+				old = newEntity!!,
+				user = UUID.randomUUID().toString()))
+
 		val list = dao!!.listById("43dcc6e7-cfcd-44af-a4e5-bbe8f7d948cc".toUUID())
-		Assert.assertEquals(1, list.size)
+		Assert.assertEquals(3, list.size)
 
 	}
 }
