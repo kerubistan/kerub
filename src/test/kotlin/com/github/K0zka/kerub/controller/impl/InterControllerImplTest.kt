9c868eb36f4496b9365408a045554bc5f53a73a0
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-09-06 17:54:45 +0200

    mockito -> mockito-kotlin
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/K0zka/kerub/controller/impl/InterControllerImplTest.kt
+++ src/test/kotlin/com/github/K0zka/kerub/controller/impl/InterControllerImplTest.kt
@@ -3,23 +3,20 @@ package com.github.K0zka.kerub.controller.impl
 import com.github.K0zka.kerub.controller.EntityEventMessage
 import com.github.K0zka.kerub.model.Project
 import com.github.K0zka.kerub.model.messages.EntityAddMessage
+import com.nhaarman.mockito_kotlin.mock
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.runners.MockitoJUnitRunner
 import org.springframework.jms.core.JmsTemplate
 import java.util.Date
 import java.util.UUID
 
-@RunWith(MockitoJUnitRunner::class) class InterControllerImplTest {
-	@Mock
-	var jmsTemplate: JmsTemplate? = null
+class InterControllerImplTest {
+	var jmsTemplate: JmsTemplate = mock()
 	var interControllerImpl: InterControllerImpl? = null
 
 	@Before
 	fun setup() {
-		interControllerImpl = InterControllerImpl(jmsTemplate!!)
+		interControllerImpl = InterControllerImpl(jmsTemplate)
 	}
 
 	@Test
