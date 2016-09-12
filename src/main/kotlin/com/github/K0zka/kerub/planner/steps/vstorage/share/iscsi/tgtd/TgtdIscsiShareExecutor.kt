02b90818d3cf450ecea80e4fdda4041221c81f09
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-09-12 18:04:22 +0200

    fix: create the configuration record if it does not exist
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/planner/steps/vstorage/share/iscsi/tgtd/TgtdIscsiShareExecutor.kt
+++ src/main/kotlin/com/github/K0zka/kerub/planner/steps/vstorage/share/iscsi/tgtd/TgtdIscsiShareExecutor.kt
@@ -3,6 +3,7 @@ package com.github.K0zka.kerub.planner.steps.vstorage.share.iscsi.tgtd
 import com.github.K0zka.kerub.data.config.HostConfigurationDao
 import com.github.K0zka.kerub.host.HostCommandExecutor
 import com.github.K0zka.kerub.host.HostManager
+import com.github.K0zka.kerub.model.config.HostConfiguration
 import com.github.K0zka.kerub.model.services.IscsiService
 import com.github.K0zka.kerub.planner.execution.AbstractStepExecutor
 import com.github.K0zka.kerub.utils.genPassword
@@ -14,18 +15,19 @@ class TgtdIscsiShareExecutor(
 		private val hostManager: HostManager)
 : AbstractStepExecutor<TgtdIscsiShare, String>() {
 	override fun update(step: TgtdIscsiShare, updates: String) {
-		hostConfigDao.update(step.host.id) {
-			config ->
-			config.copy(
-					services = config.services + IscsiService(
-							vstorageId = step.vstorage.id,
-							password = updates
-					)
-			)
-		}
+		val config = hostConfigDao[step.host.id] ?: HostConfiguration(id = step.host.id)
+		config.copy()
+		hostConfigDao.update(
+				config.copy(
+						services = config.services + IscsiService(
+								vstorageId = step.vstorage.id,
+								password = updates
+						)
+				)
+		)
 	}
 
-	override fun perform(step: TgtdIscsiShare) : String {
+	override fun perform(step: TgtdIscsiShare): String {
 		val password = genPassword()
 		hostExecutor.execute(step.host) {
 			session ->
