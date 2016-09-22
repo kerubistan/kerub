b6347bc6ba865e9696bc24f9ccc6bdb1cc1971d9
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-09-22 21:57:33 +0200

    mockito -> kotlin-mockito
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/K0zka/kerub/host/ControllerManagerImplTest.kt
+++ src/test/kotlin/com/github/K0zka/kerub/host/ControllerManagerImplTest.kt
@@ -3,6 +3,7 @@ package com.github.K0zka.kerub.host
 import com.github.K0zka.kerub.data.dynamic.ControllerDynamicDao
 import com.github.K0zka.kerub.matchAny
 import com.github.K0zka.kerub.model.dynamic.ControllerDynamic
+import com.nhaarman.mockito_kotlin.mock
 import org.hamcrest.CoreMatchers
 import org.infinispan.manager.EmbeddedCacheManager
 import org.infinispan.remoting.transport.Address
@@ -13,35 +14,32 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.runners.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class) class ControllerManagerImplTest {
+class ControllerManagerImplTest {
 
-	@Mock
-	var dao : ControllerDynamicDao? = null
+	val dao : ControllerDynamicDao = mock()
 
-	@Mock
-	var cacheManager : EmbeddedCacheManager? = null
+	val cacheManager : EmbeddedCacheManager = mock()
 
-	@Mock
-	var address : Address? = null
+	var address : Address = mock()
 
 	@Test
 	fun getControllerId() {
-		Mockito.`when`(cacheManager?.address).thenReturn(address)
-		Mockito.`when`(address?.toString()).thenReturn("PASS")
-		Assert.assertThat(ControllerManagerImpl(dao!!, cacheManager!!).getControllerId(), CoreMatchers.`is`("PASS"))
+		Mockito.`when`(cacheManager.address).thenReturn(address)
+		Mockito.`when`(address.toString()).thenReturn("PASS")
+		Assert.assertThat(ControllerManagerImpl(dao, cacheManager).getControllerId(), CoreMatchers.`is`("PASS"))
 	}
 
 	@Test
 	fun start() {
-		Mockito.`when`(cacheManager?.address).thenReturn(address)
+		Mockito.`when`(cacheManager.address).thenReturn(address)
 		val notNullInstance = ControllerDynamic("", 1, 0, listOf())
-		Mockito.`when`(dao?.add(matchAny(ControllerDynamic::class.java, notNullInstance)))
+		Mockito.`when`(dao.add(matchAny(ControllerDynamic::class.java, notNullInstance)))
 			.then {
 				val controllerDynamic = it.arguments[0]!! as ControllerDynamic
 				//checks
 				""
 			}
-		ControllerManagerImpl(dao!!, cacheManager!!).start()
+		ControllerManagerImpl(dao, cacheManager).start()
 		Mockito.verify(dao)?.add(matchAny(ControllerDynamic::class.java, notNullInstance))
 	}
 }
