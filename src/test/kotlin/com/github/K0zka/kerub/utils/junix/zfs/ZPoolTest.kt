a97fab6242f1c3a772e263ace9c2579608eddd40
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-10-10 19:21:24 +0200

    zpool list utility
    Bug-Url: https://github.com/kerubistan/kerub/issues/174
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/test/kotlin/com/github/K0zka/kerub/utils/junix/zfs/ZPoolTest.kt
@@ -0,0 +1,36 @@
+package com.github.K0zka.kerub.utils.junix.zfs
+
+import com.github.K0zka.kerub.utils.resource
+import com.github.K0zka.kerub.utils.toSize
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.apache.commons.io.input.NullInputStream
+import org.apache.sshd.client.channel.ChannelExec
+import org.apache.sshd.client.future.OpenFuture
+import org.apache.sshd.client.session.ClientSession
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ZPoolTest {
+
+	val session: ClientSession = mock()
+	val openFuture: OpenFuture = mock()
+	val channelExec: ChannelExec = mock()
+
+	@Test
+	fun listWithFreeBsd11() {
+		whenever(session.createExecChannel(any())).thenReturn(channelExec)
+		whenever(channelExec.open()).thenReturn(openFuture)
+		whenever(channelExec.invertedOut).thenReturn(resource("com/github/K0zka/kerub/utils/junix/zfs/zpool-list-freebsd11.txt"))
+		whenever(channelExec.invertedErr).thenReturn(NullInputStream(0))
+
+		val zpools = ZPool.list(session)
+		assertEquals(2, zpools.size)
+		assertEquals("test", zpools[0].name)
+		assertFalse(zpools[0].readOnly)
+		assertEquals("14561816953702211935", zpools[0].id)
+		assertEquals("1.35M".toSize(), zpools[0].allocated)
+	}
+}
