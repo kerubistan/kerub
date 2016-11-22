17cb59064c1df73f0d45c8e207a82170ca9eb1fc
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-11-22 07:34:36 +0100

    test for the vm references
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/model/expectations/VmDependency.kt
+++ src/main/kotlin/com/github/K0zka/kerub/model/expectations/VmDependency.kt
@@ -5,8 +5,8 @@ import com.github.K0zka.kerub.model.ExpectationLevel
 import java.util.UUID
 
 @JsonTypeName("vm-dependency")
-data class VmDependency(override val level: ExpectationLevel, val otherVm: List<UUID>)
+data class VmDependency(override val level: ExpectationLevel = ExpectationLevel.DealBreaker, val otherVm: UUID)
 : VirtualMachineExpectation, VirtualMachineReference {
 	override val referredVmIds: List<UUID>
-		get() = listOf()
+		get() = listOf(otherVm)
 }
---
+++ src/test/kotlin/com/github/K0zka/kerub/model/VirtualMachineTest.kt
@@ -0,0 +1,46 @@
+package com.github.K0zka.kerub.model
+
+import com.github.K0zka.kerub.model.expectations.NotSameHostExpectation
+import com.github.K0zka.kerub.model.expectations.VmDependency
+import com.github.K0zka.kerub.model.io.BusType
+import com.github.K0zka.kerub.model.io.DeviceType
+import com.github.K0zka.kerub.testDisk
+import com.github.K0zka.kerub.testVm
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.util.UUID
+
+class VirtualMachineTest {
+
+	@Test
+	fun references() {
+		val otherVmId = UUID.randomUUID()
+		val thirdVmId = UUID.randomUUID()
+		val refs = testVm.copy(
+				expectations = listOf(
+						NotSameHostExpectation(
+								otherVmId = otherVmId
+						),
+						VmDependency(
+								otherVm = thirdVmId
+						)
+				),
+				virtualStorageLinks = listOf(
+						VirtualStorageLink(
+								virtualStorageId = testDisk.id,
+								bus = BusType.sata,
+								device = DeviceType.disk
+						)
+				)
+		).references()
+
+		assertEquals(2, refs.keys.size)
+		val vmRefs = refs[VirtualMachine::class]!!
+		assertEquals(2, vmRefs.size)
+		assertTrue(vmRefs.contains(otherVmId))
+		assertTrue(vmRefs.contains(thirdVmId))
+		val vStorageRefs = refs[VirtualStorageDevice::class]!!
+		assertEquals(vStorageRefs, listOf(testDisk.id))
+	}
+}
