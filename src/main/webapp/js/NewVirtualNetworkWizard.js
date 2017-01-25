33c0e4c5fa951afbce8e7ecd32b42b4973d12059
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-01-25 20:48:51 +0100

    warn if the vnet name is already used
    Bug-Url: https://github.com/kerubistan/kerub/issues/173

--- src/main/webapp/js/NewVirtualNetworkWizard.js
+++ src/main/webapp/js/NewVirtualNetworkWizard.js
@@ -7,12 +7,28 @@ var NewVirtualNetworkWizard = function($scope, $uibModalInstance, $log, appsessi
 		expectations : []
 	};
 
+	$scope.validating = false;
+	$scope.valid = true;
+
 	$scope.addVirtualNetwork = function() {
 		$scope.inprg = true;
 		appsession.put('/s/r/vnet', $scope.vnet).then(function() {
 			$scope.inprg = false;
 			$uibModalInstance.dismiss();
 		});
+	};
+
+	$scope.checkVirtualNetworkName = function() {
+		var name = $scope.vnet.name;
+		if(name.length > 0) {
+			appsession.get('s/r/vnet/byname/' + name).success(function(results) {
+				if(name == $scope.vnet.name) {
+					$scope.valid = results.length == 0;
+				}
+			});
+		} else {
+			$scope.valid = false;
+		}
 	};
 
 	$scope.close = function() {
