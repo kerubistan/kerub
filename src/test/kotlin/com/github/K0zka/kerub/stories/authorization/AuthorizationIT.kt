ea569469ef73b4f08e7b3b3ed1f3ca72ee49509b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-02-22 21:52:58 +0100

    added json output
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/K0zka/kerub/stories/authorization/AuthorizationIT.kt
+++ src/test/kotlin/com/github/K0zka/kerub/stories/authorization/AuthorizationIT.kt
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
-		plugin = arrayOf("pretty", "html:target/test-reports/planner"),
+		plugin = arrayOf(
+				"pretty",
+				"html:target/test-reports/planner",
+				"json:target/test-reports/planner/cucumber.json"
+		),
 		features = arrayOf(
 				"classpath:stories/general/authorization/accounts.feature",
 				"classpath:stories/general/authorization/projects.feature"
