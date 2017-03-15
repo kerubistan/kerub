8725725c9eab198a4981177190e75242779a7e93
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-03-15 07:35:37 +0100

    typo: seesion -> session
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/WebSocketUtils.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/WebSocketUtils.kt
@@ -3,12 +3,12 @@ package com.github.K0zka.kerub.utils
 import org.springframework.web.socket.WebSocketSession
 
 private val doubleQuote = "\""
-private val seesionID = "JSESSIONID="
+private val sessionID = "JSESSIONID="
 
 val WebSocketSession.servletSessionId: String
 	get() =
-	requireNotNull(this.handshakeHeaders["COOKIE"]?.firstOrNull { it.contains(seesionID) }
-			?.substringAfter(seesionID)
+	requireNotNull(this.handshakeHeaders["COOKIE"]?.firstOrNull { it.contains(sessionID) }
+			?.substringAfter(sessionID)
 			?.substringBefore(";")
 			?.removePrefix(doubleQuote)
 			?.removeSuffix(doubleQuote)) { "session ID should be set" }
