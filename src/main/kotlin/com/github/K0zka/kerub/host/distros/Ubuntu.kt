48a112384609c2f7247ed63dd7c499c159ff4503
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-04-14 12:36:57 +0200

    ubuntu-17 seem to work fine with the Ubuntu adapter
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/host/distros/Ubuntu.kt
+++ src/main/kotlin/com/github/K0zka/kerub/host/distros/Ubuntu.kt
@@ -6,5 +6,5 @@ import com.github.K0zka.kerub.utils.silent
 
 class Ubuntu : AbstractDebian("Ubuntu") {
 	override fun handlesVersion(version: Version): Boolean =
-			silent { version.major.toInt().between(12, 14) } ?: false
+			silent { version.major.toInt().between(12, 17) } ?: false
 }
