6d69456032748eb001a524f9d0c1fdcec13b178b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-05-23 12:56:45 +0200

    utility on netty to integrate with kovenant
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/main/kotlin/com/github/K0zka/kerub/utils/ipmi/IOUtils.kt
@@ -0,0 +1,18 @@
+package com.github.K0zka.kerub.utils.ipmi
+
+import io.netty.channel.Channel
+import io.netty.channel.ChannelFuture
+import nl.komponents.kovenant.Promise
+import nl.komponents.kovenant.deferred
+
+fun ChannelFuture.then(timeout: Int, action: (Channel) -> Unit): Promise<Channel, Exception> =
+		deferred<Channel, Exception>().let {
+			deferred ->
+			this.addListener {
+				val channel = (it as ChannelFuture).channel()
+				action(channel)
+				deferred.resolve(channel)
+			}
+			deferred.promise
+		}
+
---
+++ src/test/kotlin/com/github/K0zka/kerub/utils/ipmi/IOUtilsKtTest.kt
@@ -0,0 +1,29 @@
+package com.github.K0zka.kerub.utils.ipmi
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.netty.channel.Channel
+import io.netty.channel.ChannelFuture
+import io.netty.util.concurrent.GenericFutureListener
+import org.junit.Test
+
+class IOUtilsKtTest {
+	@Test
+	fun then() {
+		val channelFuture = mock<ChannelFuture>()
+		val future = mock<ChannelFuture>()
+		val channel = mock<Channel>()
+		whenever(future.channel()).thenReturn(channel)
+		whenever(channelFuture.addListener(any())).then {
+			(it.arguments[0] as GenericFutureListener<ChannelFuture>).operationComplete(future)
+			channelFuture
+		}
+		val action = mock<(Channel) -> Unit>()
+		channelFuture.then(1000, action)
+
+		verify(action).invoke(eq(channel))
+	}
+}
