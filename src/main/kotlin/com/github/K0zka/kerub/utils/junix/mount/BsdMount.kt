0f2a97c625dabeeb6430742ac3dc55dfae17b4a6
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-05-29 07:35:41 +0200

    redundant safe call removed
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/utils/junix/mount/BsdMount.kt
+++ src/main/kotlin/com/github/K0zka/kerub/utils/junix/mount/BsdMount.kt
@@ -10,8 +10,7 @@ import org.apache.sshd.client.session.ClientSession
 
 object BsdMount : OsCommand {
 	override fun available(hostCapabilities: HostCapabilities?): Boolean
-			= hostCapabilities?.os == OperatingSystem.BSD && hostCapabilities?.distribution?.name == "FreeBSD"
-
+			= hostCapabilities?.os == OperatingSystem.BSD && hostCapabilities.distribution?.name == "FreeBSD"
 
 	fun listMounts(session: ClientSession): List<FsMount> =
 			session.executeOrDie("mount").lines().map {
