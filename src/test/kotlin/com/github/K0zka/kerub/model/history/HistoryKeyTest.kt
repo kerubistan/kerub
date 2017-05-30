54f5c7bbdee48309f1c37070ba5a8ffc8e69286a
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-05-30 07:25:03 +0200

    cleanup: explicit type argument
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/K0zka/kerub/model/history/HistoryKeyTest.kt
+++ src/test/kotlin/com/github/K0zka/kerub/model/history/HistoryKeyTest.kt
@@ -6,21 +6,21 @@ import org.junit.Test
 class HistoryKeyTest {
 	@Test
 	fun testToString() {
-		Assert.assertTrue(HistoryKey<String>("foo").toString().startsWith("foo - "))
-		Assert.assertTrue(HistoryKey<String>("foo", 0).toString().startsWith("foo - 19"))
+		Assert.assertTrue(HistoryKey("foo").toString().startsWith("foo - "))
+		Assert.assertTrue(HistoryKey("foo", 0).toString().startsWith("foo - 19"))
 	}
 
 	@Test
 	fun testEquals() {
-		Assert.assertEquals( HistoryKey<String>("foo", 0), HistoryKey<String>("foo", 0) )
-		Assert.assertNotEquals( HistoryKey<String>("foo", 0), HistoryKey<String>("Bar", 0) )
-		Assert.assertNotEquals( HistoryKey<String>("foo", 0), null )
-		Assert.assertNotEquals( HistoryKey<String>("foo", 0), HistoryKey<String>("foo", 1) )
-		Assert.assertNotEquals( HistoryKey<String>("foo", 0), HistoryKey<Int>(4, 0) )
+		Assert.assertEquals(HistoryKey("foo", 0), HistoryKey("foo", 0))
+		Assert.assertNotEquals(HistoryKey("foo", 0), HistoryKey("Bar", 0))
+		Assert.assertNotEquals(HistoryKey("foo", 0), null)
+		Assert.assertNotEquals(HistoryKey("foo", 0), HistoryKey("foo", 1))
+		Assert.assertNotEquals(HistoryKey("foo", 0), HistoryKey(4, 0))
 	}
 
 	@Test
 	fun testHashCode() {
-		Assert.assertEquals(HistoryKey<String>("foo", 0).hashCode(), HistoryKey<String>("foo", 0).hashCode())
+		Assert.assertEquals(HistoryKey("foo", 0).hashCode(), HistoryKey("foo", 0).hashCode())
 	}
 }
