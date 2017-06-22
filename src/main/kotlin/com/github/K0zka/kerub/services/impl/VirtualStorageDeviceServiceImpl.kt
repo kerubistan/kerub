5def106913bb84e7c75be6bef6640e72c4e08df9
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-06-22 17:39:53 +0200

    upload to fs device
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/K0zka/kerub/services/impl/VirtualStorageDeviceServiceImpl.kt
+++ src/main/kotlin/com/github/K0zka/kerub/services/impl/VirtualStorageDeviceServiceImpl.kt
@@ -6,6 +6,7 @@ import com.github.K0zka.kerub.data.dynamic.VirtualStorageDeviceDynamicDao
 import com.github.K0zka.kerub.host.HostCommandExecutor
 import com.github.K0zka.kerub.model.VirtualStorageDevice
 import com.github.K0zka.kerub.model.dynamic.VirtualStorageDeviceDynamic
+import com.github.K0zka.kerub.model.dynamic.VirtualStorageFsAllocation
 import com.github.K0zka.kerub.model.dynamic.VirtualStorageGvinumAllocation
 import com.github.K0zka.kerub.model.dynamic.VirtualStorageLvmAllocation
 import com.github.K0zka.kerub.model.expectations.StorageAvailabilityExpectation
@@ -63,6 +64,9 @@ class VirtualStorageDeviceServiceImpl(
 			}
 			is VirtualStorageGvinumAllocation -> {
 				uploadRaw(data, device, "/dev/gvinum/${device.id}", session)
+			}
+			is VirtualStorageFsAllocation -> {
+				uploadRaw(data, device, "${dyn.allocation.mountPoint}/${device.id}", session)
 			}
 			else -> {
 				TODO()
