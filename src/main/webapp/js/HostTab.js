15ebf65c4072ce697ea0da581695f3a8f394a134
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-10-10 07:47:23 +0200

    probably more human-friendly way of refreshing the items in the table
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/webapp/js/HostTab.js
+++ src/main/webapp/js/HostTab.js
@@ -24,8 +24,28 @@ kerubApp.controller('HostTab', function($scope, $uibModal, $log, socket, appsess
 		});
 	}
 	$scope.refresh();
+
     socket.subscribe('/host', function(msg) {
-        $scope.refresh();
+    	$log.info(msg);
+    	switch(msg['@type']) {
+    		case 'entity-remove':
+    			$log.info('remove ' + msg.obj.id);
+    			$scope.refresh();
+    			break;
+    		case 'entity-update':
+    			var updatedHostId = msg.obj.id;
+    			for(var idx = 0; idx < $scope.hosts.result.size; idx ++) {
+    				if($scope.hosts.result[idx].id === id) {
+    					$scope.hosts.result[idx] = msg.obj;
+    				}
+    			}
+    			break;
+    		case 'entity-add':
+    			if($scope.hosts.result.length < $scope.itemsPerPage) {
+					$scope.hosts.result.push(msg.obj);
+    			}
+    			break;
+    	}
     }, 'HostTab');
 
 
