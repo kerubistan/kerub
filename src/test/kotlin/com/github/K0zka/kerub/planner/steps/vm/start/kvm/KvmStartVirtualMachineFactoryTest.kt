295b86a3dcbbab02b4a193200f4c62378af62292
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-10-12 07:27:55 +0200

    cleanup: code formatted, always true statement removed
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/K0zka/kerub/planner/steps/vm/start/kvm/KvmStartVirtualMachineFactoryTest.kt
+++ src/test/kotlin/com/github/K0zka/kerub/planner/steps/vm/start/kvm/KvmStartVirtualMachineFactoryTest.kt
@@ -15,69 +15,69 @@ import com.github.K0zka.kerub.model.expectations.VirtualMachineAvailabilityExpec
 import com.github.K0zka.kerub.model.hardware.ProcessorInformation
 import com.github.K0zka.kerub.planner.OperationalState
 import com.github.K0zka.kerub.utils.toSize
-import org.junit.Assert
 import org.junit.Test
 import java.util.UUID
+import kotlin.test.assertTrue
 
 class KvmStartVirtualMachineFactoryTest {
 
-	val vm = VirtualMachine(
+	private val vm = VirtualMachine(
 			name = "test-vm"
-	                       )
-	val host = Host(
+	)
+	private val host = Host(
 			address = "host-1.example.com",
 			dedicated = true,
 			id = UUID.randomUUID(),
 			publicKey = "",
-	        capabilities = HostCapabilities(
-			        cpus = listOf(ProcessorInformation(
-					        coreCount = 2,
-			                threadCount = 4,
-			                maxSpeedMhz = 3000,
-			                flags = listOf("vmx"),
-			                manufacturer = "TEST",
-			                version = "TEST CPU",
-			                socket = "",
-			                voltage = null
-			                                          )),
-	                cpuArchitecture = "x86_64",
-	                totalMemory = "8 GB".toSize(),
-	                installedSoftware = listOf(
+			capabilities = HostCapabilities(
+					cpus = listOf(ProcessorInformation(
+							coreCount = 2,
+							threadCount = 4,
+							maxSpeedMhz = 3000,
+							flags = listOf("vmx"),
+							manufacturer = "TEST",
+							version = "TEST CPU",
+							socket = "",
+							voltage = null
+					)),
+					cpuArchitecture = "x86_64",
+					totalMemory = "8 GB".toSize(),
+					installedSoftware = listOf(
 							SoftwarePackage(name = "qemu-kvm", version = Version.fromVersionString("2.4.1")),
 							SoftwarePackage(name = "libvirt", version = Version.fromVersionString("1.2.18"))
 					),
-	                devices = listOf(),
-	                os = OperatingSystem.Linux,
-	                system = null,
-	                chassis = null,
-	                distribution = null
-	                                       )
-	               )
+					devices = listOf(),
+					os = OperatingSystem.Linux,
+					system = null,
+					chassis = null,
+					distribution = null
+			)
+	)
 
-	val hostDyn = HostDynamic(
+	private val hostDyn = HostDynamic(
 			id = host.id,
 			status = HostStatus.Up,
-	        memFree = "8 GB".toSize()
-	                         )
+			memFree = "8 GB".toSize()
+	)
 
 
-	val vmDyn = VirtualMachineDynamic(
+	private val vmDyn = VirtualMachineDynamic(
 			id = vm.id,
 			status = VirtualMachineStatus.Up,
 			hostId = host.id,
 			memoryUsed = "1 GB".toSize()
-	                                 )
+	)
 
 	@Test
 	fun produceWithoutBlankState() {
 		val steps = KvmStartVirtualMachineFactory.produce(OperationalState.fromLists())
-		Assert.assertTrue(steps.isEmpty())
+		assertTrue(steps.isEmpty())
 	}
 
 	@Test
 	fun produceWithoutHost() {
 		val steps = KvmStartVirtualMachineFactory.produce(OperationalState.fromLists())
-		Assert.assertTrue(steps.isEmpty())
+		assertTrue(steps.isEmpty())
 	}
 
 	@Test
@@ -87,7 +87,7 @@ class KvmStartVirtualMachineFactoryTest {
 						vms = listOf(vm),
 						hosts = listOf(host),
 						vmDyns = listOf()))
-		Assert.assertTrue(steps.isEmpty())
+		assertTrue(steps.isEmpty())
 	}
 
 	@Test
@@ -97,13 +97,13 @@ class KvmStartVirtualMachineFactoryTest {
 						vms = listOf(vm.copy(
 								expectations = listOf(VirtualMachineAvailabilityExpectation(
 										up = true,
-								        level = ExpectationLevel.DealBreaker
-								                                                    )
-						                    ))),
+										level = ExpectationLevel.DealBreaker
+								)
+								))),
 						hosts = listOf(host),
 						vmDyns = listOf(vmDyn),
 						hostDyns = listOf(hostDyn)))
-		Assert.assertTrue(steps.isEmpty())
+		assertTrue(steps.isEmpty())
 	}
 
 	@Test
@@ -112,18 +112,19 @@ class KvmStartVirtualMachineFactoryTest {
 				expectations = listOf(VirtualMachineAvailabilityExpectation(
 						up = true,
 						level = ExpectationLevel.DealBreaker
-				                                                           )
-				                     ))
+				)
+				))
 		val steps = KvmStartVirtualMachineFactory.produce(
 				OperationalState.fromLists(
 						vms = listOf(vmToRun),
 						hosts = listOf(host),
 						vmDyns = listOf(),
 						hostDyns = listOf(hostDyn)))
-		Assert.assertTrue(steps.isNotEmpty())
-		Assert.assertTrue(steps.any { it is KvmStartVirtualMachine
-				&& it.vm == vmToRun
-				&& it.host == host } )
+		assertTrue(steps.isNotEmpty())
+		assertTrue(steps.any {
+			it.vm == vmToRun
+					&& it.host == host
+		})
 	}
 
 }
