e20237abf721bcd8ca0cae9161d024db7a0e5eb1
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2017-10-27 17:58:59 +0200

    cpu type detection fix for gentoo
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/host/distros/Gentoo.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/host/distros/Gentoo.kt
@@ -1,6 +1,7 @@
 package com.github.kerubistan.kerub.host.distros
 
 import com.github.kerubistan.kerub.host.PackageManager
+import com.github.kerubistan.kerub.host.execute
 import com.github.kerubistan.kerub.host.packman.EmergePackageManager
 import com.github.kerubistan.kerub.model.Version
 import org.apache.sshd.client.session.ClientSession
@@ -17,5 +18,7 @@ class Gentoo : LsbDistribution("Gentoo") {
 	override fun installMonitorPackages(session: ClientSession) {
 		//do nothing, at the moment it is not possible to install monitoring packages on gentoo
 	}
+
+	override fun detectHostCpuType(session: ClientSession): String = session.execute("uname -m").trim()
 
 }
