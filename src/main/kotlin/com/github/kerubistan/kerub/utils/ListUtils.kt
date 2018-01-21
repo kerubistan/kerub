1b0d841006f1050ab58d1002878cc273d488999b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-01-21 09:05:56 +0100

    performance improvement on the frequently used list join extension method
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/ListUtils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/ListUtils.kt
@@ -15,7 +15,13 @@ fun <T> List<T>.skip(): List<T> =
 
 fun <T> Collection<Collection<T>>.join(): List<T> {
 	var result = listOf<T>()
-	this.forEach { result += it }
+	this.forEach {
+		//performance: the += operation is relatively expensive, even with empty lists
+		//checking for empty improves throughput from 1.5M ops/sec to 1.8M ops/sec
+		if (it.isNotEmpty()) {
+			result += it
+		}
+	}
 	return result
 }
 
