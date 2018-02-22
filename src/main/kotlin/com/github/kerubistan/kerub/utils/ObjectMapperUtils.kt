0ac33ddd9d5e917cafdc4d9bcb40fd615c177989
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-02-22 21:18:32 +0100

    cleanup - just for one line less
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/ObjectMapperUtils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/ObjectMapperUtils.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
-fun createObjectMapper(prettyPrint: Boolean = true): ObjectMapper {
-	return ObjectMapper().registerModule(KotlinModule()).registerModule(AfterburnerModule())
-			.configure(SerializationFeature.INDENT_OUTPUT, prettyPrint)
-}
+fun createObjectMapper(prettyPrint: Boolean = true): ObjectMapper =
+		ObjectMapper().registerModule(KotlinModule()).registerModule(AfterburnerModule())
+				.configure(SerializationFeature.INDENT_OUTPUT, prettyPrint)
