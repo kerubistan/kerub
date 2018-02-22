01ee1af021aaeb3b3c5d3b802b26906de48d5ca3
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-02-22 21:07:31 +0100

    afterburner registered in objectmapper
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/ObjectMapperUtils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/ObjectMapperUtils.kt
@@ -2,9 +2,10 @@ package com.github.kerubistan.kerub.utils
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 fun createObjectMapper(prettyPrint: Boolean = true): ObjectMapper {
-	return ObjectMapper().registerModule(KotlinModule())
+	return ObjectMapper().registerModule(KotlinModule()).registerModule(AfterburnerModule())
 			.configure(SerializationFeature.INDENT_OUTPUT, prettyPrint)
 }
