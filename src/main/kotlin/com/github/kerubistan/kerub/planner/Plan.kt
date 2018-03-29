08479d30675de37065a653c9feecae9ba6b6b1d7
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-03-29 07:24:47 +0200

    tiny: simplification on the plan reservations function
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/Plan.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/Plan.kt
@@ -2,16 +2,12 @@ package com.github.kerubistan.kerub.planner
 
 import com.github.kerubistan.kerub.planner.reservations.Reservation
 import com.github.kerubistan.kerub.planner.steps.AbstractOperationalStep
+import com.github.kerubistan.kerub.utils.join
 
 data class Plan(
 		val state: OperationalState,
 		val steps: List<AbstractOperationalStep> = listOf()
 ) {
-	fun reservations(): Collection<Reservation<*>> {
-		var result = setOf<Reservation<*>>()
-		steps.forEach {
-			result += it.reservations()
-		}
-		return result
-	}
+	fun reservations(): Collection<Reservation<*>> =
+			steps.map { it.reservations() }.join().toSet()
 }
