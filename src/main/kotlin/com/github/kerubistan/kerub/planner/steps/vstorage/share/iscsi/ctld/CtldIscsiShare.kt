bd9581d2dedaf6e7bcaa8a88d1e4c8bde0595c1a
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-04-29 12:34:59 +0200

    steps should be data classes
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/vstorage/share/iscsi/ctld/CtldIscsiShare.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/vstorage/share/iscsi/ctld/CtldIscsiShare.kt
@@ -4,4 +4,4 @@ import com.github.kerubistan.kerub.model.Host
 import com.github.kerubistan.kerub.model.VirtualStorageDevice
 import com.github.kerubistan.kerub.planner.steps.vstorage.share.iscsi.AbstractIscsiShare
 
-class CtldIscsiShare(override val host: Host, override val vstorage: VirtualStorageDevice) : AbstractIscsiShare
+data class CtldIscsiShare(override val host: Host, override val vstorage: VirtualStorageDevice) : AbstractIscsiShare
