bb7c88ff1106fc11c8825db126eab3e9ffe295d6
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-04-29 21:58:28 +0200

    tests for inverse removal
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/planner/PlanRationalizerImplTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/planner/PlanRationalizerImplTest.kt
@@ -19,6 +19,66 @@ import kotlin.test.assertTrue
 class PlanRationalizerImplTest {
 
 	@Test
+	fun tryRemoveInverses() {
+		val vm = testVm.copy(expectations = listOf(VirtualMachineAvailabilityExpectation()))
+		val startVirtualMachine = KvmStartVirtualMachine(vm = vm, host = testHost)
+		assertTrue("remove the fully irrelevant steps from after and before") {
+			val plan = Plan.planBy(
+					initial = OperationalState.fromLists(
+							hosts = listOf(testHost),
+							hostDyns = listOf(HostDynamic(id = testHost.id, status = HostStatus.Up)),
+							vms = listOf(vm)
+					),
+					steps = listOf(
+							StartNfsDaemon(host = testHost),
+							startVirtualMachine,
+							StopNfsDaemon(host = testHost)
+					)
+			)
+
+			val rational = PlanRationalizerImpl(mock(), mock(), mock()).tryRemoveInverses(plan)
+			rational.steps == listOf(startVirtualMachine)
+		}
+
+		assertTrue("remove the fully irrelevant steps from before") {
+			val plan = Plan.planBy(
+					initial = OperationalState.fromLists(
+							hosts = listOf(testHost),
+							hostDyns = listOf(HostDynamic(id = testHost.id, status = HostStatus.Up)),
+							vms = listOf(vm)
+					),
+					steps = listOf(
+							StartNfsDaemon(host = testHost),
+							StopNfsDaemon(host = testHost),
+							startVirtualMachine
+					)
+			)
+
+			val rational = PlanRationalizerImpl(mock(), mock(), mock()).tryRemoveInverses(plan)
+			rational.steps == listOf(startVirtualMachine)
+		}
+
+		assertTrue("remove the fully irrelevant steps from after") {
+			val plan = Plan.planBy(
+					initial = OperationalState.fromLists(
+							hosts = listOf(testHost),
+							hostDyns = listOf(HostDynamic(id = testHost.id, status = HostStatus.Up)),
+							vms = listOf(vm)
+					),
+					steps = listOf(
+							startVirtualMachine,
+							StartNfsDaemon(host = testHost),
+							StopNfsDaemon(host = testHost)
+					)
+			)
+
+			val rational = PlanRationalizerImpl(mock(), mock(), mock()).tryRemoveInverses(plan)
+			rational.steps == listOf(startVirtualMachine)
+		}
+
+	}
+
+	@Test
 	fun rationalize() {
 		assertTrue("there is not much left to rationalize on a blank plan") {
 			val blank = Plan(
