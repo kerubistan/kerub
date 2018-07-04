cb6d18d28fc6a118379c2adaada428e048e50f7a
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-07-04 17:40:33 +0200

    just comment
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/Utils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/Utils.kt
@@ -1,5 +1,9 @@
 package com.github.kerubistan.kerub.planner.steps
 
+/**
+ * Convenience method for step factories to generate steps only on a condition (usually some flag in the controller
+ * config)
+ */
 inline fun <T> factoryFeature(enabled: Boolean, producer: () -> List<T>): List<T> =
 		if (enabled) {
 			producer()
