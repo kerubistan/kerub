e54046aff667d37d5bdcb4157c63cef0314893e4
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-08-09 07:20:21 +0200

    negative test for truncate junix utility
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/utils/junix/truncate/TruncateTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/utils/junix/truncate/TruncateTest.kt
@@ -1,12 +1,14 @@
 package com.github.kerubistan.kerub.utils.junix.truncate
 
 import com.github.kerubistan.kerub.GB
+import com.github.kerubistan.kerub.TB
 import com.github.kerubistan.kerub.sshtestutils.mockCommandExecution
 import com.nhaarman.mockito_kotlin.argThat
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import org.apache.sshd.client.session.ClientSession
 import org.junit.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertTrue
 
 class TruncateTest {
@@ -26,4 +28,15 @@ class TruncateTest {
 		Truncate.truncate(session = session, path = "/kerub/test.raw", virtualSize = 100.GB)
 		verify(session).createExecChannel(argThat { startsWith("truncate") })
 	}
+
+	@Test
+	fun truncateWithError() {
+		session.mockCommandExecution("truncate -s \\d+ .*".toRegex(),
+				error = "truncate: failed to truncate 'test3' at 109951162777600 bytes: File too large")
+		assertThrows<Exception> {
+			Truncate.truncate(session = session, path = "/kerub/test.raw", virtualSize = 100.TB)
+		}
+		verify(session).createExecChannel(argThat { startsWith("truncate") })
+	}
+
 }
