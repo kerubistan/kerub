3c9b6cb0f10ce93eba105434a64edb2a1283fd6c
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-08-09 18:21:49 +0200

    test implementation - and actual fixes - for NotSameStorageExpectationViolationDetector
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/issues/violations/vstorage/NotSameStorageExpectationViolationDetector.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/issues/violations/vstorage/NotSameStorageExpectationViolationDetector.kt
@@ -1,6 +1,7 @@
 package com.github.kerubistan.kerub.planner.issues.violations.vstorage
 
 import com.github.kerubistan.kerub.model.VirtualStorageDevice
+import com.github.kerubistan.kerub.model.dynamic.VirtualStorageDeviceDynamic
 import com.github.kerubistan.kerub.model.expectations.NotSameStorageExpectation
 import com.github.kerubistan.kerub.planner.OperationalState
 import com.github.kerubistan.kerub.planner.issues.violations.VStorageViolationDetector
@@ -8,11 +9,15 @@ import com.github.kerubistan.kerub.planner.issues.violations.VStorageViolationDe
 object NotSameStorageExpectationViolationDetector : VStorageViolationDetector<NotSameStorageExpectation> {
 	override fun check(entity: VirtualStorageDevice, expectation: NotSameStorageExpectation,
 					   state: OperationalState): Boolean {
+		fun VirtualStorageDeviceDynamic.hostIds() = this.allocations.map { it.hostId }
 		val diskDyn = state.vStorage[entity.id]?.dynamic
 		return diskDyn == null || expectation.otherDiskId.let {
 			otherVdiskId ->
 			val otherDiskDyn = state.vStorage[otherVdiskId]?.dynamic
-			otherDiskDyn == null || otherDiskDyn.allocation.hostId != diskDyn.allocation.hostId
+			otherDiskDyn == null
+					|| otherDiskDyn.allocations.isEmpty()
+					|| diskDyn.allocations.isEmpty()
+					|| !diskDyn.hostIds().containsAll( otherDiskDyn.hostIds() )
 		}
 
 	}
--- src/test/kotlin/com/github/kerubistan/kerub/planner/issues/violations/vstorage/NotSameStorageExpectationViolationDetectorTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/planner/issues/violations/vstorage/NotSameStorageExpectationViolationDetectorTest.kt
@@ -1,10 +1,149 @@
 package com.github.kerubistan.kerub.planner.issues.violations.vstorage
 
+import com.github.kerubistan.kerub.GB
+import com.github.kerubistan.kerub.model.dynamic.VirtualStorageDeviceDynamic
+import com.github.kerubistan.kerub.model.dynamic.VirtualStorageFsAllocation
+import com.github.kerubistan.kerub.model.expectations.NotSameStorageExpectation
+import com.github.kerubistan.kerub.model.io.VirtualDiskFormat
+import com.github.kerubistan.kerub.planner.OperationalState
+import com.github.kerubistan.kerub.testDisk
+import com.github.kerubistan.kerub.testHost
 import org.junit.Test
+import java.util.UUID
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class NotSameStorageExpectationViolationDetectorTest {
 	@Test
 	fun check() {
+
+		assertTrue("blank state should be fine") {
+			NotSameStorageExpectationViolationDetector
+					.check(testDisk, NotSameStorageExpectation(otherDiskId = testDisk.id), OperationalState.fromLists())
+		}
+
+		assertTrue("other disk not allocated yet") {
+			val secondDisk = testDisk.copy(
+					id = UUID.randomUUID(),
+					expectations = listOf()
+			)
+			val notSameStorageExpectation = NotSameStorageExpectation(otherDiskId = secondDisk.id)
+			val firstDisk = testDisk.copy(
+					id = UUID.randomUUID(),
+					expectations = listOf(notSameStorageExpectation)
+			)
+			NotSameStorageExpectationViolationDetector.check(
+					firstDisk,
+					notSameStorageExpectation,
+					OperationalState.fromLists(
+							vStorage = listOf(firstDisk, secondDisk),
+							vStorageDyns = listOf(
+									VirtualStorageDeviceDynamic(
+											id = firstDisk.id,
+											allocations = listOf(
+													VirtualStorageFsAllocation(
+															actualSize = 10.GB,
+															fileName = "first-disk.raw",
+															type = VirtualDiskFormat.raw,
+															mountPoint = "/test-mount-1",
+															hostId = testHost.id
+													)
+											)
+									),
+									VirtualStorageDeviceDynamic(
+											id = secondDisk.id,
+											allocations = listOf()
+									)
+							),
+							hosts = listOf(testHost)
+					)
+			)
+		}
+
+		assertTrue("constrained disk not allocated yet") {
+			val secondDisk = testDisk.copy(
+					id = UUID.randomUUID(),
+					expectations = listOf()
+			)
+			val notSameStorageExpectation = NotSameStorageExpectation(otherDiskId = secondDisk.id)
+			val firstDisk = testDisk.copy(
+					id = UUID.randomUUID(),
+					expectations = listOf(notSameStorageExpectation)
+			)
+			NotSameStorageExpectationViolationDetector.check(
+					firstDisk,
+					notSameStorageExpectation,
+					OperationalState.fromLists(
+							vStorage = listOf(firstDisk, secondDisk),
+							vStorageDyns = listOf(
+									VirtualStorageDeviceDynamic(
+											id = firstDisk.id,
+											allocations = listOf(
+											)
+									),
+									VirtualStorageDeviceDynamic(
+											id = secondDisk.id,
+											allocations = listOf(
+													VirtualStorageFsAllocation(
+															actualSize = 10.GB,
+															fileName = "second-disk.raw",
+															type = VirtualDiskFormat.raw,
+															mountPoint = "/test-mount-2",
+															hostId = testHost.id
+													)
+											)
+									)
+							),
+							hosts = listOf(testHost)
+					)
+			)
+		}
+
+		assertFalse("disks on the same host") {
+			val secondDisk = testDisk.copy(
+					id = UUID.randomUUID(),
+					expectations = listOf()
+			)
+			val notSameStorageExpectation = NotSameStorageExpectation(otherDiskId = secondDisk.id)
+			val firstDisk = testDisk.copy(
+					id = UUID.randomUUID(),
+					expectations = listOf(notSameStorageExpectation)
+			)
+			NotSameStorageExpectationViolationDetector.check(
+					firstDisk,
+					notSameStorageExpectation,
+					OperationalState.fromLists(
+							vStorage = listOf(firstDisk, secondDisk),
+							vStorageDyns = listOf(
+									VirtualStorageDeviceDynamic(
+											id = firstDisk.id,
+											allocations = listOf(
+													VirtualStorageFsAllocation(
+															actualSize = 10.GB,
+															fileName = "first-disk.raw",
+															type = VirtualDiskFormat.raw,
+															mountPoint = "/test-mount-1",
+															hostId = testHost.id
+													)
+											)
+									),
+									VirtualStorageDeviceDynamic(
+											id = secondDisk.id,
+											allocations = listOf(
+													VirtualStorageFsAllocation(
+															actualSize = 10.GB,
+															fileName = "second-disk.qcow2",
+															type = VirtualDiskFormat.qcow2,
+															mountPoint = "/test-mount-2",
+															hostId = testHost.id
+													)
+											)
+									)
+							),
+							hosts = listOf(testHost)
+					)
+			)
+		}
 	}
 
 }
