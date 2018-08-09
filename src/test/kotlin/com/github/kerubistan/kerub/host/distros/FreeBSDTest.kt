9e1410b05d60ffb676ac306838269bb92a91e6be
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-08-09 21:59:03 +0200

    simplification on the ssh command testing
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/host/distros/FreeBSDTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/host/distros/FreeBSDTest.kt
@@ -1,24 +1,16 @@
 package com.github.kerubistan.kerub.host.distros
 
 import com.github.kerubistan.kerub.model.Version
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.eq
+import com.github.kerubistan.kerub.sshtestutils.mockCommandExecution
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
-import org.apache.commons.io.input.NullInputStream
-import org.apache.sshd.client.channel.ChannelExec
-import org.apache.sshd.client.future.OpenFuture
 import org.apache.sshd.client.session.ClientSession
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.io.ByteArrayInputStream
 
 class FreeBSDTest {
 
-	val session : ClientSession = mock()
-	val future : OpenFuture = mock()
-	val exec : ChannelExec = mock()
+	private val session: ClientSession = mock()
 
 	@Test
 	fun handlesVersion() {
@@ -28,42 +20,22 @@ class FreeBSDTest {
 
 	@Test
 	fun detect() {
-		whenever(session.createExecChannel(eq("uname -s"))).thenReturn(exec)
-		whenever(exec.open()).thenReturn(future)
-		whenever(exec.invertedErr).thenReturn(NullInputStream(0))
-		whenever(exec.invertedOut).thenReturn(
-				ByteArrayInputStream("FreeBSD".toByteArray())
-		)
-
+		session.mockCommandExecution("uname -s", output = "FreeBSD")
 		assertTrue(FreeBSD().detect(session))
 	}
 
 	@Test
 	fun detectHostCpuType() {
-		whenever(session.createExecChannel(eq("uname -p"))).thenReturn(exec)
-		whenever(exec.open()).thenReturn(future)
-		whenever(exec.invertedErr).thenReturn(NullInputStream(0))
-		whenever(exec.invertedOut).thenReturn(
-				ByteArrayInputStream("amd64".toByteArray())
-		)
-
+		session.mockCommandExecution("uname -p", output = "amd64")
 		assertEquals("X86_64", FreeBSD().detectHostCpuType(session))
 	}
 
 	@Test
 	fun getTotalMemory() {
-		whenever(session.createExecChannel(any())).thenReturn(exec)
-		whenever(exec.open()).thenReturn(future)
-		whenever(exec.invertedErr).thenReturn(NullInputStream(0))
-		whenever(exec.invertedOut).thenReturn(
-				ByteArrayInputStream("hw.physmem: 1040261120".toByteArray())
-		)
-
+		session.mockCommandExecution("sysctl hw.physmem", output = "hw.physmem: 1040261120")
 		val totalMem = FreeBSD().getTotalMemory(session)
-
 		assertEquals(1040261120.toBigInteger(), totalMem)
 	}
-
 
 
 }
