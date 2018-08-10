48d59b2f3234d870aa131e17a44fad8920ae39cf
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-08-10 07:20:53 +0200

    do not assume single allocation
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/hypervisor/kvm/Utils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/hypervisor/kvm/Utils.kt
@@ -33,84 +33,79 @@ private fun storageToXml(
 			</disk>
 """
 
-private fun kvmDeviceType(linkInfo: VirtualStorageLinkInfo, targetHost: Host): String {
-	if (remoteHost(linkInfo, targetHost)) {
-		return "network"
-	} else {
-		return allocationTypeToDiskType[linkInfo.allocation.javaClass.kotlin] ?: TODO()
-	}
-}
-
-fun allocationType(deviceDyn: VirtualStorageLinkInfo): String {
-	return deviceDyn.allocation.let {
-		when (it) {
-			is VirtualStorageLvmAllocation -> "raw"
-			is VirtualStorageFsAllocation -> it.type.name.toLowerCase()
-			else -> TODO("")
+private fun kvmDeviceType(linkInfo: VirtualStorageLinkInfo, targetHost: Host): String =
+		if (remoteHost(linkInfo, targetHost)) {
+			"network"
+		} else {
+			allocationTypeToDiskType[linkInfo.allocation.javaClass.kotlin] ?: TODO()
 		}
+
+fun allocationType(deviceDyn: VirtualStorageLinkInfo): String = deviceDyn.allocation.let {
+	when (it) {
+		is VirtualStorageLvmAllocation -> "raw"
+		is VirtualStorageFsAllocation -> it.type.name.toLowerCase()
+		else -> TODO("")
 	}
 }
 
-fun allocationToXml(linkInfo: VirtualStorageLinkInfo, targetHost: Host): String {
-	return if (remoteHost(linkInfo, targetHost)) {
-		"""
-		<source protocol='iscsi' name='${iscsiStorageId(linkInfo.device.stat.id)}/1'>
-			<host name='${linkInfo.storageHost.stat.address}' port='3260' />
-		</source>
-		<auth username="$iscsiUser">
-			<secret type='iscsi' uuid-'${linkInfo.device.stat.id}'/>
-		</auth>
-		"""
-	} else {
-		val allocation = requireNotNull(linkInfo.device.dynamic).allocation
-		when (allocation) {
-			is VirtualStorageFsAllocation ->
-				"<source file='${allocation.mountPoint}/${linkInfo.device.stat.id}'/>"
-			is VirtualStorageLvmAllocation ->
-				"<source dev='${allocation.path}'/>"
-			else -> TODO()
+fun allocationToXml(linkInfo: VirtualStorageLinkInfo, targetHost: Host): String =
+		if (remoteHost(linkInfo, targetHost)) {
+			"""
+			<source protocol='iscsi' name='${iscsiStorageId(linkInfo.device.stat.id)}/1'>
+				<host name='${linkInfo.storageHost.stat.address}' port='3260' />
+			</source>
+			<auth username="$iscsiUser">
+				<secret type='iscsi' uuid-'${linkInfo.device.stat.id}'/>
+			</auth>
+			"""
+		} else {
+			val allocation = linkInfo.allocation
+			when (allocation) {
+				is VirtualStorageFsAllocation ->
+					"<source file='${allocation.mountPoint}/${allocation.fileName}'/>"
+				is VirtualStorageLvmAllocation ->
+					"<source dev='${allocation.path}'/>"
+				else -> TODO()
+			}
 		}
-	}
-}
 
-private fun remoteHost(linkInfo: VirtualStorageLinkInfo, targetHost: Host) = linkInfo.device.dynamic?.allocation?.hostId != targetHost.id
+private fun remoteHost(linkInfo: VirtualStorageLinkInfo, targetHost: Host) = linkInfo.allocation.hostId != targetHost.id
 
-fun vmDefinitiontoXml(vm: VirtualMachine, disks: List<VirtualStorageLinkInfo>, password: String, targetHost: Host): String {
-	return """
-<domain type='kvm'>
-    <name>${vm.id}</name>
-    <uuid>${vm.id}</uuid>
-    <memory unit='B'>${vm.memory.min}</memory>
-	<memtune>
-		<hard_limit unit='B'>${vm.memory.min}</hard_limit>
-	</memtune>
-	<memoryBacking>
-		<allocation mode="ondemand"/>
-	</memoryBacking>
-    <vcpu>${vm.nrOfCpus}</vcpu>
-    <os>
-    	<type arch='x86_64'>hvm</type>
-        <boot dev='hd'/>
-        <boot dev='cdrom'/>
-    </os>
-    <features>
-        <acpi/>
-        <apic/>
-        <pae/>
-        <hap/>
-    </features>
-    <devices>
-		<input type='keyboard' bus='ps2'/>
-		<graphics type='spice' autoport='yes' listen='0.0.0.0' passwd='$password'>
-			<listen type='address' address='0.0.0.0'/>
-			<image compression='off'/>
-		</graphics>
-		<video>
-			<model type='qxl' ram='65536' vram='65536' vgamem='16384' heads='1'/>
-			<address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
-		</video>
-		${storagesToXml(disks, targetHost)}
-    </devices>
-</domain>
-"""
-}
+fun vmDefinitiontoXml(vm: VirtualMachine, disks: List<VirtualStorageLinkInfo>, password: String, targetHost: Host): String =
+		"""
+	<domain type='kvm'>
+		<name>${vm.id}</name>
+		<uuid>${vm.id}</uuid>
+		<memory unit='B'>${vm.memory.min}</memory>
+		<memtune>
+			<hard_limit unit='B'>${vm.memory.min}</hard_limit>
+		</memtune>
+		<memoryBacking>
+			<allocation mode="ondemand"/>
+		</memoryBacking>
+		<vcpu>${vm.nrOfCpus}</vcpu>
+		<os>
+			<type arch='x86_64'>hvm</type>
+			<boot dev='hd'/>
+			<boot dev='cdrom'/>
+		</os>
+		<features>
+			<acpi/>
+			<apic/>
+			<pae/>
+			<hap/>
+		</features>
+		<devices>
+			<input type='keyboard' bus='ps2'/>
+			<graphics type='spice' autoport='yes' listen='0.0.0.0' passwd='$password'>
+				<listen type='address' address='0.0.0.0'/>
+				<image compression='off'/>
+			</graphics>
+			<video>
+				<model type='qxl' ram='65536' vram='65536' vgamem='16384' heads='1'/>
+				<address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+			</video>
+			${storagesToXml(disks, targetHost)}
+		</devices>
+	</domain>
+	"""
