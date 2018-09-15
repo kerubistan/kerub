f10fbe6f4cb21b2b7b13873b67cd15a088e01202
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-09-15 07:03:05 +0200

    another serialization thing
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/model/collection/DataCollection.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/model/collection/DataCollection.kt
@@ -2,8 +2,9 @@ package com.github.kerubistan.kerub.model.collection
 
 import com.github.kerubistan.kerub.model.Entity
 import com.github.kerubistan.kerub.model.dynamic.DynamicEntity
+import java.io.Serializable
 
-interface DataCollection<S : Entity<*>, D : DynamicEntity> {
+interface DataCollection<S : Entity<*>, D : DynamicEntity> : Serializable {
 	val stat: S
 	val dynamic: D?
 }
