6203077d771c5223f9b059b5005d2d5a30870b2b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-09-21 17:16:15 +0200

    cleanup: const values
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/junix/storagemanager/lvm/Constants.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/junix/storagemanager/lvm/Constants.kt
@@ -1,7 +1,7 @@
 package com.github.kerubistan.kerub.utils.junix.storagemanager.lvm
 
-val separator = "--end"
-val fields = "lv_uuid,lv_name,lv_path,lv_size,raid_min_recovery_rate,raid_max_recovery_rate,lv_layout,data_percent"
-val vgFields = "vg_uuid,vg_name,vg_size,vg_free,vg_extent_count,vg_free_count"
-val fieldSeparator = ":"
-val listOptions = "--noheadings --units b --separator=$fieldSeparator"
+const val separator = "--end"
+const val fields = "lv_uuid,lv_name,lv_path,lv_size,raid_min_recovery_rate,raid_max_recovery_rate,lv_layout,data_percent"
+const val vgFields = "vg_uuid,vg_name,vg_size,vg_free,vg_extent_count,vg_free_count"
+const val fieldSeparator = ":"
+const val listOptions = "--noheadings --units b --separator=$fieldSeparator"
