f4cd11c0cbf4a99bf9d0416fb71583de51dd3b41
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-09-27 17:13:46 +0200

    handle the UndeclaredThrowableException - and all in a single mapper
    Bug-Url: https://github.com/kerubistan/kerub/issues/236
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/services/exc/mappers/DefaultExceptionMapper.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/services/exc/mappers/DefaultExceptionMapper.kt
@@ -1,10 +1,80 @@
 package com.github.kerubistan.kerub.services.exc.mappers
 
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.github.kerubistan.kerub.services.exc.HostAddressException
+import org.apache.shiro.authc.AuthenticationException
+import org.apache.shiro.authz.AuthorizationException
+import org.apache.shiro.authz.UnauthenticatedException
+import org.apache.sshd.common.RuntimeSshException
+import org.apache.sshd.common.SshException
+import java.lang.reflect.UndeclaredThrowableException
+import java.nio.channels.UnresolvedAddressException
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status.FORBIDDEN
+import javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR
+import javax.ws.rs.core.Response.Status.NOT_ACCEPTABLE
+import javax.ws.rs.core.Response.Status.UNAUTHORIZED
 import javax.ws.rs.ext.ExceptionMapper
 
 class DefaultExceptionMapper : ExceptionMapper<Throwable> {
-	override fun toResponse(exception: Throwable): Response {
-		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(RestError("INT1", "Internal error.")).build()
+
+	companion object {
+		val jsonMappingError = RestError("MAP1", "Mapping error.")
+		val jsonParseError = RestError("MAP2", "Json parsing error")
 	}
+
+	override fun toResponse(exception: Throwable): Response =
+			when (exception) {
+				is UnresolvedAddressException ->
+					Response.status(NOT_ACCEPTABLE)
+							.entity(RestError("HOST2", "Address can not be resolved"))
+							.build()
+				is UnauthenticatedException ->
+					Response.status(UNAUTHORIZED)
+							.entity(RestError("AUTH1", "Authentication needed"))
+							.build()
+				is RuntimeSshException ->
+					Response.status(NOT_ACCEPTABLE)
+							.entity(RestError("HOST1", "Host could not be connected"))
+							.build()
+				is UndeclaredThrowableException ->
+					toResponse(exception.undeclaredThrowable)
+				is HostAddressException ->
+					Response.status(NOT_ACCEPTABLE)
+							.entity(RestError("HOST2", "Host address not valid"))
+							.build()
+				is JsonMappingException ->
+					Response.status(NOT_ACCEPTABLE)
+							.entity(jsonMappingError)
+							.header("Content-Type", APPLICATION_JSON)
+							.build()
+				is JsonParseException ->
+					Response.status(NOT_ACCEPTABLE)
+							.entity(jsonParseError)
+							.header("Content-Type", APPLICATION_JSON)
+							.build()
+				is AuthenticationException ->
+					Response.status(UNAUTHORIZED)
+							.entity(RestError("AUTH2", "Authentication needed"))
+							.build()
+				is AuthorizationException ->
+					Response.status(FORBIDDEN)
+							.entity(RestError("SEC1", "Access denied"))
+							.build()
+				is SshException ->
+					Response.status(NOT_ACCEPTABLE)
+							.entity(RestError(
+											code = "HOST3",
+											message = exception.message
+									))
+							.header("Content-Type", APPLICATION_JSON)
+							.build()
+				else ->
+					Response.status(INTERNAL_SERVER_ERROR)
+							.entity(RestError("INT1", "Internal error."))
+							.build()
+
+			}
 }
--- src/test/kotlin/com/github/kerubistan/kerub/services/exc/mappers/DefaultExceptionMapperTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/services/exc/mappers/DefaultExceptionMapperTest.kt
@@ -1,8 +1,17 @@
 package com.github.kerubistan.kerub.services.exc.mappers
 
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.nhaarman.mockito_kotlin.mock
+import org.apache.http.HttpStatus
+import org.apache.sshd.common.RuntimeSshException
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
 import org.junit.Test
+import java.nio.channels.UnresolvedAddressException
 import javax.ws.rs.core.Response
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class DefaultExceptionMapperTest {
 	@Test
@@ -10,4 +19,35 @@ class DefaultExceptionMapperTest {
 		val response = DefaultExceptionMapper().toResponse(NullPointerException("Surprise!"))
 		assertEquals(Response.Status.INTERNAL_SERVER_ERROR.statusCode, response.status)
 	}
+
+	@Test
+	fun toResponseWithJsonParseException() {
+		val response = DefaultExceptionMapper().toResponse(JsonParseException("TEST", null))
+		Assert.assertThat(response, CoreMatchers.notNullValue())
+		Assert.assertThat(response.status, CoreMatchers.`is`(HttpStatus.SC_NOT_ACCEPTABLE))
+	}
+
+	@Test
+	fun toResponseWithJsonMappingException() {
+		val exception = mock<JsonMappingException>()
+		val response = DefaultExceptionMapper().toResponse(exception)
+		assertEquals(Response.Status.NOT_ACCEPTABLE.statusCode, response.status)
+		assertNotNull(response.entity)
+	}
+
+	@Test
+	fun toResponseWithRuntimeSshException() {
+		val response = DefaultExceptionMapper().toResponse(RuntimeSshException("foo"))
+		Assert.assertThat(response, CoreMatchers.notNullValue())
+		Assert.assertEquals(response.status, Response.Status.NOT_ACCEPTABLE.statusCode)
+		Assert.assertNotNull(response.entity)
+	}
+
+	@Test
+	fun toResponseWithUnresolvedAddressException() {
+		val response = DefaultExceptionMapper().toResponse(UnresolvedAddressException())
+		Assert.assertEquals(response.status, Response.Status.NOT_ACCEPTABLE.statusCode)
+		Assert.assertNotNull(response.entity)
+	}
+
 }
