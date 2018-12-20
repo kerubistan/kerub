a5b48cb4301b1bbedf9b27d42b65905d3a02fcc2
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-12-20 14:14:07 +0100

    cleanup (sonar auto-bugs)
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/AnyUtils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/AnyUtils.kt
@@ -4,6 +4,7 @@ fun <T> T.equalsAnyOf(vararg others: T) = others.any { this == it }
 
 fun String.equalsAnyIgnoreCase(vararg others: String) = others.any { it.equals(this, ignoreCase = true) }
 
+/**
+ * Proudly and intentionally doing nothing.
+ */
 inline fun NOP(): Unit {}
-
-inline fun NOP(comment: String): Unit {}
