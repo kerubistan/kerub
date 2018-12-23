d7fa670588f8056bd6c01ed0884087d5660535a0
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-12-23 20:02:48 +0100

    test on json serialization - was broken once, let's be on the safe side
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/test/kotlin/com/github/kerubistan/kerub/model/HostJsonSerializationTest.kt
@@ -0,0 +1,37 @@
+package com.github.kerubistan.kerub.model
+
+import com.github.kerubistan.kerub.TB
+import com.github.kerubistan.kerub.model.hardware.BlockDevice
+import com.github.kerubistan.kerub.testHost
+import com.github.kerubistan.kerub.testHostCapabilities
+import com.github.kerubistan.kerub.utils.createObjectMapper
+import org.junit.Test
+import java.util.UUID
+import kotlin.test.assertEquals
+
+class HostJsonSerializationTest {
+	@Test
+	fun serializeDeserialize() {
+		val host = testHost.copy(
+				capabilities = testHostCapabilities.copy(
+						blockDevices = listOf(
+								BlockDevice(deviceName = "sda", storageCapacity = 2.TB),
+								BlockDevice(deviceName = "sdb", storageCapacity = 2.TB),
+								BlockDevice(deviceName = "sdc", storageCapacity = 2.TB)
+						),
+						storageCapabilities = listOf(
+								LvmStorageCapability(
+										physicalVolumes = listOf(2.TB, 2.TB, 2.TB),
+										size = 6.TB,
+										volumeGroupName = "test-vg",
+										id = UUID.randomUUID()
+								)
+						)
+				)
+		)
+		val objectMapper = createObjectMapper(prettyPrint = true)
+		val serializedHost = objectMapper.writeValueAsString(host)
+		val deserializedHost = objectMapper.readValue(serializedHost, Host::class.java)
+		assertEquals(host, deserializedHost)
+	}
+}
