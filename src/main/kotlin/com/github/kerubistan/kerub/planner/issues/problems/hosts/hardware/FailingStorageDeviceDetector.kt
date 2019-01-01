255ea07674fe663dd6200bc2ea702ebfafb2ec22
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-01-01 23:12:51 +0100

    some simplification and tuning on the FailingStorageDeviceDetector
    Bug-Url: https://github.com/kerubistan/kerub/issues/242
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/issues/problems/hosts/hardware/FailingStorageDeviceDetector.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/issues/problems/hosts/hardware/FailingStorageDeviceDetector.kt
@@ -8,17 +8,17 @@ import com.github.kerubistan.kerub.utils.join
 
 object FailingStorageDeviceDetector : ProblemDetector<FailingStorageDevice> {
 	override fun detect(plan: Plan) = plan.state.hosts.values.mapNotNull { host: HostDataCollection ->
-		host.dynamic?.storageDeviceHealth?.filter { (_, healthy) -> !healthy }?.map {(device , _) ->
-
-			host.stat.capabilities?.storageCapabilities?.filterIsInstance<VolumeManagerStorageCapability>()?.filter {
-				device in it.storageDevices
-			}?.map {
-				FailingStorageDevice(
-						device = device,
-						storageCapability = it,
-						host = host.stat
-				)
-			}
+		host.dynamic?.storageDeviceHealth?.mapNotNull { (device, healthy) ->
+			if (healthy) null else
+				host.stat.capabilities?.storageCapabilities?.filterIsInstance<VolumeManagerStorageCapability>()?.filter {
+					device in it.storageDevices
+				}?.map {
+					FailingStorageDevice(
+							device = device,
+							storageCapability = it,
+							host = host.stat
+					)
+				}
 		}
-	}.join().filterNotNull().join()
+	}.join().join()
 }
