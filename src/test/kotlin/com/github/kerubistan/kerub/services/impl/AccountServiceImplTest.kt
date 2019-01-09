32e81ca58d902bb49cd8d3d0d56ba20ea099c332
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-01-09 21:10:03 +0100

    tests disabled - not implemented
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/services/impl/AccountServiceImplTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/services/impl/AccountServiceImplTest.kt
@@ -1,15 +1,25 @@
 package com.github.kerubistan.kerub.services.impl
 
+import com.github.kerubistan.kerub.data.AccountDao
+import com.github.kerubistan.kerub.security.AssetAccessController
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.AssumptionViolatedException
 import org.junit.Test
 
 class AccountServiceImplTest {
 
+	private val dao = mock<AccountDao>()
+	private val accessController = mock<AssetAccessController>()
+
 	@Test
 	fun search() {
+		val service = AccountServiceImpl(dao, accessController)
+		throw AssumptionViolatedException("not implemented")
 	}
 
 	@Test
 	fun getById() {
-		AccountServiceImpl::class.java.methods.forEach { println("$it ${it.isSynthetic}") }
+		val service = AccountServiceImpl(dao, accessController)
+		throw AssumptionViolatedException("not implemented")
 	}
 }
