fc835207d2d981dbcae20fc32a87b0f76d9c764a
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-01-22 16:31:44 +0100

    reuse index in GenerateSshKeyFactory
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/host/security/generate/GenerateSshKeyFactory.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/host/security/generate/GenerateSshKeyFactory.kt
@@ -1,7 +1,6 @@
 package com.github.kerubistan.kerub.planner.steps.host.security.generate
 
 import com.github.kerubistan.kerub.model.Expectation
-import com.github.kerubistan.kerub.model.dynamic.HostStatus
 import com.github.kerubistan.kerub.planner.OperationalState
 import com.github.kerubistan.kerub.planner.issues.problems.Problem
 import com.github.kerubistan.kerub.planner.steps.AbstractOperationalStepFactory
@@ -11,7 +10,7 @@ object GenerateSshKeyFactory : AbstractOperationalStepFactory<GenerateSshKey>() 
 	override val problemHints = setOf<KClass<out Problem>>()
 	override val expectationHints = setOf<KClass<out Expectation>>()
 
-	override fun produce(state: OperationalState): List<GenerateSshKey> = state.hosts.values
-			.filter { it.dynamic?.status == HostStatus.Up && it.config?.publicKey == null }
+	override fun produce(state: OperationalState): List<GenerateSshKey> = state.runningHosts
+			.filter {  it.config?.publicKey == null }
 			.map { GenerateSshKey(host = it.stat) }
 }
