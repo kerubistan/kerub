bca742ad0eaf127b97bf2f31f4cf10346a530134
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-03-06 07:07:27 +0100

    more log on installing known hosts
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/host/security/install/InstallPublicKeyExecutor.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/host/security/install/InstallPublicKeyExecutor.kt
@@ -4,16 +4,21 @@ import com.github.kerubistan.kerub.data.config.HostConfigurationDao
 import com.github.kerubistan.kerub.host.HostCommandExecutor
 import com.github.kerubistan.kerub.model.config.HostConfiguration
 import com.github.kerubistan.kerub.planner.execution.AbstractStepExecutor
+import com.github.kerubistan.kerub.utils.getLogger
 import com.github.kerubistan.kerub.utils.junix.ssh.openssh.OpenSsh
 
 class InstallPublicKeyExecutor(private val hostCommandExecutor: HostCommandExecutor,
 							   private val hostCfgDao: HostConfigurationDao)
 	: AbstractStepExecutor<InstallPublicKey, Unit>(){
 
+	private val logger = getLogger(InstallPublicKeyExecutor::class)
+
 	override fun perform(step: InstallPublicKey) {
+		logger.info("installing public key on target host {}", step.targetHost.address)
 		hostCommandExecutor.execute(step.targetHost) {
 			OpenSsh.authorize(it, step.publicKey)
 		}
+		logger.info("adding known host {} on host {}", step.targetHost.address, step.sourceHost.address)
 		hostCommandExecutor.execute(step.sourceHost) {
 			OpenSsh.addKnownHost(it, step.targetHost.address, step.targetHost.publicKey)
 		}
