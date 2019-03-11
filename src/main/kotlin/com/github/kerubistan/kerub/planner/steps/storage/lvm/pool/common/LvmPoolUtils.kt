1d9cc3bab2e4947776c915f1c3cee9d4abe0151c
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-03-11 08:33:21 +0100

    thin pool allocation size percentage correction
    it is not possible to allocate 100%, space needed for meta volume
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/lvm/pool/common/LvmPoolUtils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/lvm/pool/common/LvmPoolUtils.kt
@@ -3,7 +3,7 @@ package com.github.kerubistan.kerub.planner.steps.storage.lvm.pool.common
 import com.github.kerubistan.kerub.model.LvmStorageCapability
 import com.github.kerubistan.kerub.model.StorageCapability
 
-val percents = (1..9) + (10..100).step(10)
+val percents = (1..9) + (10..90).step(10) + (91..98)
 
 fun volumeGroupId(storageCapabilities : List<StorageCapability>, vgName : String) =
 		storageCapabilities.first {
