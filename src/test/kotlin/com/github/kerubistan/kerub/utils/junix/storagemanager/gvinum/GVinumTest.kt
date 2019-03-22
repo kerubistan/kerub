eef463d7be17f0fb127e319c87e3f84ba2b4f1c4
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-03-22 18:07:59 +0100

    test cleanup for gvinum
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/GVinumTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/GVinumTest.kt
@@ -1,31 +1,25 @@
 package com.github.kerubistan.kerub.utils.junix.storagemanager.gvinum
 
-import com.github.kerubistan.kerub.utils.resource
+import com.github.kerubistan.kerub.sshtestutils.mockCommandExecution
+import com.github.kerubistan.kerub.sshtestutils.mockProcess
 import com.github.kerubistan.kerub.utils.resourceToString
 import com.github.kerubistan.kerub.utils.toSize
 import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.doAnswer
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-import org.apache.commons.io.input.NullInputStream
-import org.apache.sshd.client.channel.ChannelExec
-import org.apache.sshd.client.future.OpenFuture
 import org.apache.sshd.client.session.ClientSession
 import org.apache.sshd.client.subsystem.sftp.SftpClient
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.io.ByteArrayOutputStream
-import java.io.OutputStream
 import java.math.BigInteger
 import kotlin.test.assertFalse
 
 class GVinumTest {
 
 	val session : ClientSession = mock()
-	val execChannel : ChannelExec = mock()
-	val future : OpenFuture = mock()
 	val ftp : SftpClient = mock()
 
 	@Test
@@ -71,26 +65,21 @@ class GVinumTest {
 
 	@Test
 	fun listDrives() {
-		whenever(session.createExecChannel(any())).thenReturn(execChannel)
-		whenever(execChannel.open()).thenReturn(future)
-		whenever(execChannel.invertedErr).then { NullInputStream(0) }
-		whenever(execChannel.invertedOut).then {
-			resource("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/drives.txt")
-		}
+		session.mockCommandExecution(
+				"gvinum ld.*".toRegex(),
+				resourceToString("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/drives.txt")
+		)
 		val drives = GVinum.listDrives(session)
 
 		verifyDrivesList(drives)
-
 	}
 
 	@Test
 	fun listSubDisks() {
-		whenever(session.createExecChannel(any())).thenReturn(execChannel)
-		whenever(execChannel.open()).thenReturn(future)
-		whenever(execChannel.invertedErr).then { NullInputStream(0) }
-		whenever(execChannel.invertedOut).then {
-			resource("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/subdisks.txt")
-		}
+		session.mockCommandExecution(
+				"gvinum ls.*".toRegex(),
+				resourceToString("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/subdisks.txt")
+		)
 		val subdisks = GVinum.listSubDisks(session)
 
 		verifySubDisks(subdisks)
@@ -98,13 +87,10 @@ class GVinumTest {
 
 	@Test
 	fun listVolumes() {
-		whenever(session.createExecChannel(any())).thenReturn(execChannel)
-		whenever(execChannel.open()).thenReturn(future)
-		whenever(execChannel.invertedErr).then { NullInputStream(0) }
-		whenever(execChannel.invertedOut).then {
-			resource("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/volumes.txt")
-		}
-
+		session.mockCommandExecution(
+				"gvinum lv.*".toRegex(),
+				resourceToString("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/volumes.txt")
+		)
 		val volumes = GVinum.listVolumes(session)
 		verifyVolumesList(volumes)
 	}
@@ -126,12 +112,10 @@ class GVinumTest {
 
 	@Test
 	fun listPlexes() {
-		whenever(session.createExecChannel(any())).thenReturn(execChannel)
-		whenever(execChannel.open()).thenReturn(future)
-		whenever(execChannel.invertedErr).then { NullInputStream(0) }
-		whenever(execChannel.invertedOut).then {
-			resource("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/plexes.txt")
-		}
+		session.mockCommandExecution(
+				"gvinum lp.*".toRegex(),
+				resourceToString("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/plexes.txt")
+		)
 		val plexes = GVinum.listPlexes(session)
 
 		verifyPlexesList(plexes)
@@ -147,10 +131,7 @@ class GVinumTest {
 
 	@Test
 	fun createVolume() {
-		whenever(session.createExecChannel(any())).thenReturn(execChannel)
-		whenever(execChannel.open()).thenReturn(future)
-		whenever(execChannel.invertedErr).then { NullInputStream(0) }
-		whenever(execChannel.invertedOut).then { NullInputStream(0) }
+		session.mockCommandExecution("gvinum create.*".toRegex())
 		whenever(session.createSftpClient()).thenReturn(ftp)
 		val outputStream = ByteArrayOutputStream()
 		whenever(ftp.write(any())).thenReturn(outputStream)
@@ -171,12 +152,9 @@ class GVinumTest {
 
 	@Test
 	fun monitorDrives() {
-		whenever(session.createExecChannel(any())).thenReturn(execChannel)
-		whenever(execChannel.open()).thenReturn(future)
-		doAnswer {
-			resource("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/monitor-drives.txt")
-					.copyTo (it.arguments[0] as OutputStream)
-		}.`when`(execChannel)!!.out = any()
+		session.mockProcess(
+				".*".toRegex(),
+				output = resourceToString("com/github/kerubistan/kerub/utils/junix/storagemanager/gvinum/monitor-drives.txt"))
 
 		var drives : List<List<GvinumDrive>> = listOf()
 		GVinum.monitorDrives(session) {
