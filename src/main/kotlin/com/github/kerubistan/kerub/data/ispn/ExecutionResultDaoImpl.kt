0141f2f0bec10c23c026c8634584f2bda856cf5d
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-04-06 00:09:19 +0200

    some logging in case of error
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/data/ispn/ExecutionResultDaoImpl.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/data/ispn/ExecutionResultDaoImpl.kt
@@ -3,17 +3,27 @@ package com.github.kerubistan.kerub.data.ispn
 import com.github.kerubistan.kerub.data.ExecutionResultDao
 import com.github.kerubistan.kerub.model.ExecutionResult
 import com.github.kerubistan.kerub.utils.byId
+import com.github.kerubistan.kerub.utils.getLogger
 import org.infinispan.Cache
+import org.infinispan.persistence.spi.PersistenceException
 import java.util.UUID
 
-class ExecutionResultDaoImpl(private val cache: Cache<UUID, ExecutionResult>)
-	: ExecutionResultDao {
+class ExecutionResultDaoImpl(private val cache: Cache<UUID, ExecutionResult>) : ExecutionResultDao {
+
+	companion object {
+		private val logger = getLogger(ExecutionResultDaoImpl::class)
+	}
+
 	override fun addAll(entities: Collection<ExecutionResult>) {
 		cache.putAll(entities.byId())
 	}
 
 	override fun add(entity: ExecutionResult): UUID = entity.let {
-		cache[it.id] = it
+		try {
+			cache[it.id] = it
+		} catch (pe: PersistenceException) {
+			logger.error("Could not persist execution result $it ", pe)
+		}
 		it.id
 	}
 
