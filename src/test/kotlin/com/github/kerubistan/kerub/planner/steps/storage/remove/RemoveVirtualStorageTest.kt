c40e992191611691e842cb94ced7f985c5b7dd47
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-04-09 22:14:42 +0200

    disabled test - pending bugfix in kotlin-reflect
    Bug-Url: https://github.com/kerubistan/kerub/issues/256
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/planner/steps/storage/remove/RemoveVirtualStorageTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/planner/steps/storage/remove/RemoveVirtualStorageTest.kt
@@ -1,13 +1,14 @@
 package com.github.kerubistan.kerub.planner.steps.storage.remove
 
 import com.github.kerubistan.kerub.planner.OperationalState
+import com.github.kerubistan.kerub.planner.steps.AbstractOperationalStepVerifications
 import com.github.kerubistan.kerub.testDisk
 import com.github.kerubistan.kerub.testHost
 import com.github.kerubistan.kerub.testVm
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertTrue
 
-class RemoveVirtualStorageTest {
+class RemoveVirtualStorageTest : AbstractOperationalStepVerifications(RemoveVirtualStorage(testDisk)) {
 	@Test
 	fun take() {
 		val state = OperationalState.fromLists(
