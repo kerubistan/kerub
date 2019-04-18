eafaa663ba037ad6aa5c5b5876b71b1155ca18dd
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-04-18 20:22:15 +0200

    expect -> assertThrows
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/model/expectations/PoolAverageLoadExpectationTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/model/expectations/PoolAverageLoadExpectationTest.kt
@@ -1,25 +1,25 @@
 package com.github.kerubistan.kerub.model.expectations
 
-import com.github.kerubistan.kerub.expect
 import org.junit.Test
+import org.junit.jupiter.api.assertThrows
 
 class PoolAverageLoadExpectationTest {
 	@Test
 	fun validations() {
 		//maximum less than minimum
-		expect(IllegalStateException::class) {
+		assertThrows<IllegalStateException> {
 			PoolAverageLoadExpectation(max = 50, min = 60, toleranceMs = 10000)
 		}
 		//negative minimum
-		expect(IllegalStateException::class) {
+		assertThrows<IllegalStateException> {
 			PoolAverageLoadExpectation(max = 50, min = -10, toleranceMs = 10000)
 		}
 		//maximum higher than 100
-		expect(IllegalStateException::class) {
+		assertThrows<IllegalStateException> {
 			PoolAverageLoadExpectation(max = 110, min = 10, toleranceMs = 10000)
 		}
 		//negative tolerance
-		expect(IllegalStateException::class) {
+		assertThrows<IllegalStateException> {
 			PoolAverageLoadExpectation(max = 90, min = 10, toleranceMs = -10000)
 		}
 	}
