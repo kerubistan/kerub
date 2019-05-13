5bba42186cf1c186f037ad92e60dd9902587164c
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-13 23:15:43 +0200

    note: moved to kroki-utils
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/StreamUtils.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/StreamUtils.kt
@@ -2,12 +2,14 @@ package com.github.kerubistan.kerub.utils
 
 import java.nio.charset.Charset
 
+// moved to kroki
 fun resource(resource: String) =
 		requireNotNull(Thread.currentThread()
 				.contextClassLoader
 				.getResource(resource)) { "$resource not found" }
 				.openStream()!!
 
+// moved to kroki
 fun resourceToString(resource: String, charset: Charset = Charsets.UTF_8) =
 		resource(resource)
 				.use { it.reader(charset).readText() }
