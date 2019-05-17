447340ee7e8463ee66574e8d95f2c318166ef6c2
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-17 07:32:54 +0200

    few more properties for NetworkInterface
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/junix/lshw/NetworkInterface.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/junix/lshw/NetworkInterface.kt
@@ -1,6 +1,7 @@
 package com.github.kerubistan.kerub.utils.junix.lshw
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeName
 
 @JsonTypeName("network")
@@ -9,5 +10,8 @@ data class NetworkInterface @JsonCreator constructor(
 		override val configuration: Map<String, String>?,
 		override val children: List<HardwareItem>?,
 		override val description: String?,
-		val serial : String?
+		val serial : String?,
+		@JsonProperty("logicalname")
+		val logicalName : String?,
+		val capacity : Int?
 ) : HardwareItem
