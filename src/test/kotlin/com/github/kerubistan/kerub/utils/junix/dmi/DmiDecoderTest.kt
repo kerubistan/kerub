5903078d0f6b41c55622ecfaa5de292d9f7c827f
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-20 12:51:45 +0200

    a little cleanup to the dmidecoder test
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/utils/junix/dmi/DmiDecoderTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/utils/junix/dmi/DmiDecoderTest.kt
@@ -1,5 +1,8 @@
 package com.github.kerubistan.kerub.utils.junix.dmi
 
+import com.github.kerubistan.kerub.GB
+import com.github.kerubistan.kerub.KB
+import com.github.kerubistan.kerub.MB
 import com.github.kerubistan.kerub.model.hardware.ChassisInformation
 import com.github.kerubistan.kerub.model.hardware.MemoryArrayInformation
 import com.github.kerubistan.kerub.model.hardware.MemoryInformation
@@ -9,6 +12,7 @@ import com.github.kerubistan.kerub.utils.toSize
 import org.junit.Test
 import java.util.UUID
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class DmiDecoderTest {
 	@Test
@@ -19,8 +23,9 @@ class DmiDecoderTest {
 
 	@Test
 	fun type() {
-		assert(DmiDecoder.type(
-				"""
+		assert(
+				DmiDecoder.type(
+						"""
 Handle 0x002F, DMI type 41, 11 bytes
 Onboard Device
 	Reference Designation: 82567LM Gigabit Network Connection
@@ -29,23 +34,26 @@ Onboard Device
 	Type Instance: 1
 	Bus Address: 0000:00:00.1
 """
-		                      ) == 41)
+				) == 41)
 
-		assert(DmiDecoder.type(
-				"""
+		assert(
+				DmiDecoder.type(
+						"""
 Handle 0x002B, DMI type 21, 7 bytes
 Built-in Pointing Device
 	Type: Touch Pad
 	Interface: PS/2
 	Buttons: 4
 """
-		                      ) == 21)
+				) == 21)
 
 	}
 
 	@Test
 	fun handle() {
-		assert(DmiDecoder.handle("""
+		assert(
+				DmiDecoder.handle(
+						"""
 Handle 0x002B, DMI type 21, 7 bytes
 Built-in Pointing Device
 	Type: Touch Pad
@@ -58,13 +66,13 @@ Built-in Pointing Device
 	fun parse() {
 		val devices = DmiDecoder.parse(mylaptop)
 		val system = devices["0x0001"] as SystemInformation
-		assert(system.family == "IDEAPAD")
-		assert(system.version == "Lenovo G585")
-		assert(system.manufacturer == "LENOVO")
+		assertEquals("IDEAPAD", system.family)
+		assertEquals("Lenovo G585", system.version)
+		assertEquals("LENOVO", system.manufacturer)
 		val processor = devices["0x0004"] as ProcessorInformation
-		assert(processor.manufacturer == "AMD processor")
-		assert(processor.coreCount == 2)
-		assert(processor.threadCount == 2)
+		assertEquals("AMD processor", processor.manufacturer)
+		assertEquals(2, processor.coreCount)
+		assertEquals(2, processor.threadCount)
 	}
 
 	@Test
@@ -84,21 +92,21 @@ Built-in Pointing Device
 		val l1Cache = processor.l1cache!!
 		assert(l1Cache.size == "128 KB".toSize().toInt())
 		assert(l1Cache.speedNs == null)
-		assert(l1Cache.errorCorrection == "Single-bit ECC")
+		assertEquals("Single-bit ECC", l1Cache.errorCorrection)
 
 		val l2Cache = processor.l2cache!!
-		assert(l2Cache.size == "512 KB".toSize().toInt())
-		assert(l2Cache.speedNs == null)
-		assert(l2Cache.errorCorrection == "Single-bit ECC")
+		assertEquals(512.KB.toInt(), l2Cache.size.toInt())
+		assertNull(l2Cache.speedNs)
+		assertEquals("Single-bit ECC", l2Cache.errorCorrection)
 
 		val l3Cache = processor.l3cache!!
-		assert(l3Cache.size == "3072 KB".toSize().toInt())
-		assert(l3Cache.speedNs == null)
-		assert(l3Cache.errorCorrection == "Single-bit ECC")
+		assertEquals(3072.KB.toInt(), l3Cache.size)
+		assertNull(l3Cache.speedNs)
+		assertEquals("Single-bit ECC", l3Cache.errorCorrection)
 
 		val memArray = devices["0x003E"] as MemoryArrayInformation
-		assertEquals(memArray.maxCapacity, "16 GB".toSize())
-		assertEquals(memArray.errorCorrection, "None")
+		assertEquals(memArray.maxCapacity, 16.GB)
+		assertEquals("None", memArray.errorCorrection)
 	}
 
 	@Test
@@ -110,22 +118,22 @@ Built-in Pointing Device
 		assert(systemInfo.uuid == UUID.fromString("99163626-EDCF-FB4C-A81C-A7FD9EAA058E"))
 
 		val chassisInfo = devices["0x0300"] as ChassisInformation
-		assert(chassisInfo.manufacturer == "QEMU")
+		assertEquals("QEMU", chassisInfo.manufacturer)
 		assert(chassisInfo.nrOfPowerCords == null)
-		assert(chassisInfo.type == "Other")
+		assertEquals("Other", chassisInfo.type)
 
 		val procInfo = devices["0x0400"] as ProcessorInformation
-		assert(procInfo.manufacturer == "QEMU")
-		assert(procInfo.version == "pc-i440fx-2.1")
-		assert(procInfo.coreCount == 1)
-		assert(procInfo.l1cache == null)
-		assert(procInfo.l2cache == null)
-		assert(procInfo.l3cache == null)
+		assertEquals("QEMU", procInfo.manufacturer)
+		assertEquals("pc-i440fx-2.1", procInfo.version)
+		assertEquals(1, procInfo.coreCount)
+		assertNull(procInfo.l1cache)
+		assertNull(procInfo.l2cache)
+		assertNull(procInfo.l3cache)
 
 		val memInfo = devices["0x1100"] as MemoryInformation
-		assert(memInfo.manufacturer == "QEMU")
-		assert(memInfo.speedMhz == null)
-		assert(memInfo.formFactor == "DIMM")
+		assertEquals("QEMU", memInfo.manufacturer)
+		assertNull(memInfo.speedMhz)
+		assertEquals("DIMM", memInfo.formFactor)
 	}
 
 	@Test
@@ -139,15 +147,15 @@ Built-in Pointing Device
 		val devices = DmiDecoder.parse(kvmQemuBochs)
 
 		val cpuInfo = devices["0x0401"] as ProcessorInformation
-		assert(cpuInfo.manufacturer == "Bochs")
-		assert(cpuInfo.maxSpeedMhz == 2000)
+		assertEquals("Bochs", cpuInfo.manufacturer)
+		assertEquals(2000, cpuInfo.maxSpeedMhz)
 
 		val memoryDevice = devices["0x1100"] as MemoryInformation
-		assert(memoryDevice.formFactor == "DIMM")
-		assert(memoryDevice.size == "2048 MB".toSize())
+		assertEquals("DIMM", memoryDevice.formFactor)
+		assertEquals(2048.MB, memoryDevice.size)
 
 		val chassisInfo = devices["0x0300"] as ChassisInformation
-		assert(chassisInfo.manufacturer == "Bochs")
-		assert(chassisInfo.nrOfPowerCords == null)
+		assertEquals("Bochs", chassisInfo.manufacturer)
+		assertNull(chassisInfo.nrOfPowerCords)
 	}
 }
