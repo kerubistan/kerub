62db416dd2e5c1c35e67dc8ec6070b7657ed50b3
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-24 07:36:10 +0200

    mini-cleanup: no indexOf('column') for each row, for each column
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/junix/lsblk/Lsblk.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/junix/lsblk/Lsblk.kt
@@ -18,22 +18,31 @@ object Lsblk : OsCommand {
 
 	fun list(session: ClientSession, noDeps: Boolean = false): List<BlockDeviceInfo> {
 		return session.executeOrDie("lsblk -l -o NAME,ROTA,RO,RA,RM,MIN-IO,OPT-IO,TYPE,SIZE ${noDeps.flag("-d")}")
-				.lines().let {
-					val header = it.first().split(spaces).map { it.trim() }
-					val data = it.skip().filter { it.isNotBlank() }
+				.lines().let { lines ->
+					val header = lines.first().split(spaces).map { column -> column.trim() }
+					val data = lines.skip().filter { row -> row.isNotBlank() }
 
-					data.map {
-						val columns = it.split(spaces)
+					val nameIdx = header.indexOf("NAME")
+					val typeIdx = header.indexOf("TYPE")
+					val sizeIdx = header.indexOf("SIZE")
+					val minIoIdx = header.indexOf("MIN-IO")
+					val optIoIdx = header.indexOf("OPT-IO")
+					val rmIdx = header.indexOf("RM")
+					val rotaIdx = header.indexOf("ROTA")
+					val roIdx = header.indexOf("RO")
+					val raIdx = header.indexOf("RA")
+					data.map { row ->
+						val columns = row.split(spaces)
 						BlockDeviceInfo(
-								name = columns[header.indexOf("NAME")],
-								type = columns[header.indexOf("TYPE")],
-								size = columns[header.indexOf("SIZE")].toSize(),
-								minIo = columns[header.indexOf("MIN-IO")].toBigInteger(),
-								optIo = columns[header.indexOf("OPT-IO")].toBigInteger(),
-								removable = columns[header.indexOf("RM")] == TRUE,
-								rotational = columns[header.indexOf("ROTA")] == TRUE,
-								readOnly = columns[header.indexOf("RO")] == TRUE,
-								readAhead = columns[header.indexOf("RA")].toBigInteger()
+								name = columns[nameIdx],
+								type = columns[typeIdx],
+								size = columns[sizeIdx].toSize(),
+								minIo = columns[minIoIdx].toBigInteger(),
+								optIo = columns[optIoIdx].toBigInteger(),
+								removable = columns[rmIdx] == TRUE,
+								rotational = columns[rotaIdx] == TRUE,
+								readOnly = columns[roIdx] == TRUE,
+								readAhead = columns[raIdx].toBigInteger()
 						)
 					}
 				}
