8c913f6c97e7389a74bc92af1c6c8201689933f2
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-06-26 23:10:00 +0200

    moved 'take' method to the abstract base-class
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/block/copy/AbstractBlockCopy.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/block/copy/AbstractBlockCopy.kt
@@ -4,13 +4,42 @@ import com.github.kerubistan.kerub.model.StorageCapability
 import com.github.kerubistan.kerub.model.VirtualStorageDevice
 import com.github.kerubistan.kerub.model.dynamic.VirtualStorageAllocation
 import com.github.kerubistan.kerub.model.dynamic.VirtualStorageBlockDeviceAllocation
+import com.github.kerubistan.kerub.model.expectations.CloneOfStorageExpectation
+import com.github.kerubistan.kerub.planner.OperationalState
 import com.github.kerubistan.kerub.planner.steps.AbstractOperationalStep
 import com.github.kerubistan.kerub.planner.steps.storage.AbstractCreateVirtualStorage
+import com.github.kerubistan.kerub.utils.update
 
-abstract class AbstractBlockCopy  : AbstractOperationalStep {
+abstract class AbstractBlockCopy : AbstractOperationalStep {
 	abstract val sourceDevice: VirtualStorageDevice
 	abstract val targetDevice: VirtualStorageDevice
 	abstract val sourceAllocation: VirtualStorageBlockDeviceAllocation
 	abstract val allocationStep: AbstractCreateVirtualStorage<out VirtualStorageAllocation, out StorageCapability>
+
+	/*
+	this implementation only tells that after the step, the clone expectation will be cleared
+	and the new allocation will be created, this is true regardless of the further details
+	therefore the implementation can be in this abstract class
+	*/
+	override fun take(state: OperationalState): OperationalState = allocationStep.take(state).copy(
+			vStorage = state.vStorage.update(targetDevice.id) { virtualStorage ->
+				virtualStorage.copy(
+						stat = virtualStorage.stat.copy(
+								expectations = virtualStorage.stat.expectations
+										.filterNot { it is CloneOfStorageExpectation }
+						),
+						dynamic = virtualStorage.updateDynamic { dyn ->
+							dyn.copy(allocations = listOf(allocationStep.allocation))
+						}
+				)
+			}
+	)
+
+
+	protected fun validate() {
+		check(sourceDevice.id != targetDevice.id) {
+			"source device (${sourceDevice.id}) must not be the same as the target device (${targetDevice.id})"
+		}
+	}
 
 }
--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/block/copy/local/LocalBlockCopy.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/block/copy/local/LocalBlockCopy.kt
@@ -4,14 +4,11 @@ import com.github.kerubistan.kerub.model.StorageCapability
 import com.github.kerubistan.kerub.model.VirtualStorageDevice
 import com.github.kerubistan.kerub.model.dynamic.VirtualStorageAllocation
 import com.github.kerubistan.kerub.model.dynamic.VirtualStorageBlockDeviceAllocation
-import com.github.kerubistan.kerub.model.expectations.CloneOfStorageExpectation
-import com.github.kerubistan.kerub.planner.OperationalState
 import com.github.kerubistan.kerub.planner.reservations.Reservation
 import com.github.kerubistan.kerub.planner.reservations.UseHostReservation
 import com.github.kerubistan.kerub.planner.reservations.VirtualStorageReservation
 import com.github.kerubistan.kerub.planner.steps.storage.AbstractCreateVirtualStorage
 import com.github.kerubistan.kerub.planner.steps.storage.block.copy.AbstractBlockCopy
-import com.github.kerubistan.kerub.utils.update
 
 data class LocalBlockCopy(
 		override val sourceDevice: VirtualStorageDevice,
@@ -21,28 +18,12 @@ data class LocalBlockCopy(
 ) : AbstractBlockCopy() {
 
 	init {
-		check(sourceDevice.id != targetDevice.id) {
-			"source device (${sourceDevice.id}) must not be the same as the target device (${targetDevice.id})"
-		}
+		validate()
 		check(sourceAllocation.hostId == allocationStep.host.id) {
 			"source allocation host (${sourceAllocation.hostId}) must be the same as the target allocation " +
 					"host (${allocationStep.host.id})"
 		}
 	}
-
-	override fun take(state: OperationalState): OperationalState = allocationStep.take(state).copy(
-			vStorage = state.vStorage.update(targetDevice.id) { virtualStorage ->
-				virtualStorage.copy(
-						stat = virtualStorage.stat.copy(
-								expectations = virtualStorage.stat.expectations
-										.filterNot { it is CloneOfStorageExpectation }
-						),
-						dynamic = virtualStorage.updateDynamic { dyn ->
-							dyn.copy(allocations = listOf(allocationStep.allocation))
-						}
-				)
-			}
-	)
 
 	override fun reservations(): List<Reservation<*>> = listOf(
 			UseHostReservation(host = allocationStep.host),
