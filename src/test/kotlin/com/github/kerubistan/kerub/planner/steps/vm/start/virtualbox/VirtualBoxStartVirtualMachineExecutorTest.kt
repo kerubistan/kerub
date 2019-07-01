f4d567a84d9c584b29df8725d84b6b55922c1705
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-07-01 20:19:12 +0200

    implemented test VirtualBoxStartVirtualMachineExecutorTest
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/planner/steps/vm/start/virtualbox/VirtualBoxStartVirtualMachineExecutorTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/planner/steps/vm/start/virtualbox/VirtualBoxStartVirtualMachineExecutorTest.kt
@@ -1,11 +1,32 @@
 package com.github.kerubistan.kerub.planner.steps.vm.start.virtualbox
 
+import com.github.kerubistan.kerub.data.dynamic.VirtualMachineDynamicDao
+import com.github.kerubistan.kerub.host.HostCommandExecutor
+import com.github.kerubistan.kerub.host.mockHost
+import com.github.kerubistan.kerub.sshtestutils.mockCommandExecution
+import com.github.kerubistan.kerub.sshtestutils.verifyCommandExecution
+import com.github.kerubistan.kerub.testHost
+import com.github.kerubistan.kerub.testVm
+import com.nhaarman.mockito_kotlin.atLeast
+import com.nhaarman.mockito_kotlin.mock
+import org.apache.sshd.client.session.ClientSession
 import org.junit.Test
 
 class VirtualBoxStartVirtualMachineExecutorTest {
 
 	@Test
 	fun execute() {
-		TODO()
+		val hostCommandExecutor = mock<HostCommandExecutor>()
+		val vmDynDao = mock<VirtualMachineDynamicDao>()
+		val session = mock<ClientSession>()
+		session.mockCommandExecution("VBoxManage .*".toRegex())
+		hostCommandExecutor.mockHost(testHost, session)
+		VirtualBoxStartVirtualMachineExecutor(hostCommandExecutor, vmDynDao).execute(
+				VirtualBoxStartVirtualMachine(
+						host = testHost,
+						vm = testVm
+				)
+		)
+		session.verifyCommandExecution("VBoxManage .*".toRegex(), atLeast(1))
 	}
 }
