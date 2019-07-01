831121a13066f2b0f26ee7215355055adeffdc96
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-07-01 23:39:00 +0200

    unimplemented test for VirtualStorageDeviceServiceImpl
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ src/test/kotlin/com/github/kerubistan/kerub/services/impl/VirtualStorageDeviceServiceImplTest.kt
@@ -0,0 +1,41 @@
+package com.github.kerubistan.kerub.services.impl
+
+import org.junit.Test
+
+class VirtualStorageDeviceServiceImplTest {
+
+	@Test
+	fun listByOwner() {
+		TODO()
+	}
+
+	@Test
+	fun search() {
+		TODO()
+	}
+
+	@Test
+	fun add() {
+		TODO()
+	}
+
+	@Test
+	fun update() {
+		TODO()
+	}
+
+	@Test
+	fun delete() {
+		TODO()
+	}
+
+	@Test
+	fun download() {
+		TODO()
+	}
+
+	@Test
+	fun load() {
+		TODO()
+	}
+}
