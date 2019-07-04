ebfbb86241fa4e39cf4d34eb81f6c5afde7bde04
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-07-04 20:32:32 +0200

    half-finished migrate file executor
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/migrate/dead/file/MigrateFileAllocationExecutor.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/storage/migrate/dead/file/MigrateFileAllocationExecutor.kt
@@ -1,14 +1,43 @@
 package com.github.kerubistan.kerub.planner.steps.storage.migrate.dead.file
 
+import com.github.kerubistan.kerub.data.dynamic.VirtualStorageDeviceDynamicDao
+import com.github.kerubistan.kerub.host.HostCommandExecutor
+import com.github.kerubistan.kerub.planner.StepExecutor
 import com.github.kerubistan.kerub.planner.execution.AbstractStepExecutor
+import com.github.kerubistan.kerub.planner.steps.AbstractOperationalStep
+import com.github.kerubistan.kerub.utils.junix.ssh.openssh.OpenSsh
 
-class MigrateFileAllocationExecutor : AbstractStepExecutor<MigrateFileAllocation, Unit>() {
+class MigrateFileAllocationExecutor(
+		private val hostCommandExecutor: HostCommandExecutor,
+		private val virtualStorageDeviceDynamicDao: VirtualStorageDeviceDynamicDao,
+		private val stepExecutor: StepExecutor<AbstractOperationalStep>
+) : AbstractStepExecutor<MigrateFileAllocation, Unit>() {
 	override fun perform(step: MigrateFileAllocation) {
-		//OpenSsh.copyBlockDevice()
-		TODO()
+		hostCommandExecutor.execute(step.sourceHost) {session ->
+			OpenSsh.copyBlockDevice(
+				session = session,
+					sourceDevice = step.sourceAllocation.getPath(step.virtualStorage.id),
+					targetDevice = step.allocationStep.allocation.getPath(step.virtualStorage.id),
+					targetAddress = step.targetHost.address
+			)
+			stepExecutor.execute(step.deAllocationStep)
+			/*
+				TODO
+			    here we should get some device size info, which might be specific to the
+			 	logical volume technology (sparse files, thin lv, gvinum, zfs volume, etc)
+			 */
+		}
 	}
 
 	override fun update(step: MigrateFileAllocation, updates: Unit) {
-		TODO()
+		/*
+		 	TODO
+		 	here too, we should update the host state based on the info retrieved by perform
+		 */
+		virtualStorageDeviceDynamicDao.update(step.virtualStorage.id) {dyn ->
+			dyn.copy(
+					allocations = dyn.allocations - step.sourceAllocation + step.allocationStep.allocation
+			)
+		}
 	}
 }
