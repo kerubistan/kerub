7f92a0f13f58510ed6e2e180bc371c080e77841d
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-07-16 23:06:12 +0200

    long lines (sonar violation)
    https://sonarcloud.io/project/issues?id=kerubistan_kerub&issues=AWugPNdXm1kdWX7IVcLM&open=AWugPNdXm1kdWX7IVcLM
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/vm/cpuaffinity/ClearCpuAffinityExecutor.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/vm/cpuaffinity/ClearCpuAffinityExecutor.kt
@@ -3,7 +3,8 @@ package com.github.kerubistan.kerub.planner.steps.vm.cpuaffinity
 import com.github.kerubistan.kerub.host.HostCommandExecutor
 import com.github.kerubistan.kerub.planner.execution.AbstractStepExecutor
 
-class ClearCpuAffinityExecutor(private val hostExecutor: HostCommandExecutor) : AbstractStepExecutor<ClearCpuAffinity, Unit>() {
+class ClearCpuAffinityExecutor(private val hostExecutor: HostCommandExecutor) :
+		AbstractStepExecutor<ClearCpuAffinity, Unit>() {
 	override fun perform(step: ClearCpuAffinity) {
 		TODO()
 	}
