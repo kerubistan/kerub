17db9216843593e108f3a82b430fd8dca4d34284
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-09-04 16:43:25 +0200

    some more simplification on mpstat
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/junix/mpstat/MPStat.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/junix/mpstat/MPStat.kt
@@ -2,51 +2,31 @@ package com.github.kerubistan.kerub.utils.junix.mpstat
 
 import com.github.kerubistan.kerub.host.process
 import com.github.kerubistan.kerub.model.dynamic.CpuStat
+import com.github.kerubistan.kerub.utils.junix.common.MonitorOutputStream
 import com.github.kerubistan.kerub.utils.junix.common.OsCommand
 import org.apache.sshd.client.session.ClientSession
-import java.io.OutputStream
 
 object MPStat : OsCommand {
 
-	private val regex = "\\s+".toRegex()
+	private val spaces = "\\s+".toRegex()
 
-	class MPStatOutput(private val handler: (List<CpuStat>) -> Unit) : OutputStream() {
-		private val buff: StringBuilder = StringBuilder(128)
-		private var cpuStats = listOf<CpuStat>()
-		override fun write(data: Int) {
-			if (data == 10) {
-				parseOutput()
-			} else {
-				buff.append(data.toChar())
-			}
-		}
-
-		private fun parseOutput() {
-			val line = buff.toString()
-			buff.clear()
-
-			if (line.isBlank()) {
-				handler(
-						cpuStats.sortedBy { it.cpuNr }
-				)
-				cpuStats = listOf()
-			} else {
-				val fields = line.split(regex)
-				if (fields.size == 13 && fields[2] != "all" && fields[2] != "CPU") {
-					cpuStats += CpuStat(
-							cpuNr = fields[2].toInt(),
-							idle = fields[12].toFloat(),
-							ioWait = fields[6].toFloat(),
-							system = fields[5].toFloat(),
-							user = fields[3].toFloat()
-					)
-				}
-			}
-		}
-
+	private fun parseRows(rows : String) = rows.lines().filterNot { it.isBlank() }.mapNotNull { line ->
+		val fields = line.split(spaces)
+		if (fields.size == 13 && fields[2] != "all" && fields[2] != "CPU") {
+			CpuStat(
+					cpuNr = fields[2].toInt(),
+					idle = fields[12].toFloat(),
+					ioWait = fields[6].toFloat(),
+					system = fields[5].toFloat(),
+					user = fields[3].toFloat()
+			)
+		} else null
 	}
 
 	fun monitor(session: ClientSession, handler: (List<CpuStat>) -> Unit, interval: Short = 1) {
-		session.process("mpstat $interval -P ALL", MPStatOutput(handler))
+		session.process(
+				"mpstat $interval -P ALL",
+				MonitorOutputStream(separator = "\n\n", callback = handler, parser = this::parseRows)
+		)
 	}
 }
