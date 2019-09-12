9f3a06859039d4e45696b60e72edc2c7f67b8dfc
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-09-12 17:36:45 +0200

    redundant import removed
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/utils/junix/virt/virsh/Virsh.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/utils/junix/virt/virsh/Virsh.kt
@@ -1,6 +1,5 @@
 package com.github.kerubistan.kerub.utils.junix.virt.virsh
 
-import com.github.kerubistan.kerub.host.bashMonitor
 import com.github.kerubistan.kerub.host.executeOrDie
 import com.github.kerubistan.kerub.host.process
 import com.github.kerubistan.kerub.model.SoftwarePackage
