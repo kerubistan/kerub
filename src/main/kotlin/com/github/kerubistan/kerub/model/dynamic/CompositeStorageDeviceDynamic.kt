827a85afe401f7524db14da6160be07d6baa725e
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-09-18 08:08:26 +0200

    type name annotation on CompositeStorageDeviceDynamic
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/model/dynamic/CompositeStorageDeviceDynamic.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/model/dynamic/CompositeStorageDeviceDynamic.kt
@@ -1,9 +1,11 @@
 package com.github.kerubistan.kerub.model.dynamic
 
 import io.github.kerubistan.kroki.numbers.sumBy
+import org.codehaus.jackson.annotate.JsonTypeName
 import java.math.BigInteger
 import java.util.UUID
 
+@JsonTypeName("composite")
 data class CompositeStorageDeviceDynamic(
 		override val id: UUID,
 		val reportedFreeCapacity: BigInteger? = null,
