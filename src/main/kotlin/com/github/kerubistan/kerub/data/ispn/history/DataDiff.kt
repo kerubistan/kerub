d2f47b443de0deca972ac6ab02f4b05f2fbc83f9
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-09-29 01:10:15 +0200

    do not keep ignored fields in the history records
    Bug-Url: https://github.com/kerubistan/kerub/issues/262
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/data/ispn/history/DataDiff.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/data/ispn/history/DataDiff.kt
@@ -1,15 +1,21 @@
 package com.github.kerubistan.kerub.data.ispn.history
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.github.kerubistan.kerub.model.history.IgnoreDiff
 import com.github.kerubistan.kerub.model.history.PropertyChange
 import java.io.Serializable
 import kotlin.reflect.full.memberProperties
 
 inline fun <reified T : Any> diff(oldData: T, newData: T): List<PropertyChange> =
-		T::class.memberProperties.filter { it.annotations.none { it is IgnoreDiff } }.mapNotNull { property ->
+		T::class.memberProperties.filter {
+			it.annotations.none(::isIgnore)
+					&& it.getter.annotations.none(::isIgnore)
+		}.mapNotNull { property ->
 			val oldValue = property.get(oldData) as Serializable?
 			val newValue = property.get(newData) as Serializable?
 			if (oldValue != newValue) {
 				PropertyChange(property = property.name, oldValue = oldValue, newValue = newValue)
 			} else null
 		}
+
+fun isIgnore(annotation: Annotation) = annotation is IgnoreDiff || annotation is JsonIgnore
--- src/test/kotlin/com/github/kerubistan/kerub/data/ispn/history/DataDiffKtTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/data/ispn/history/DataDiffKtTest.kt
@@ -1,28 +1,79 @@
 package com.github.kerubistan.kerub.data.ispn.history
 
 import com.github.kerubistan.kerub.model.VirtualMachineStatus
+import com.github.kerubistan.kerub.model.dynamic.CompositeStorageDeviceDynamic
+import com.github.kerubistan.kerub.model.dynamic.CompositeStorageDeviceDynamicItem
 import com.github.kerubistan.kerub.model.dynamic.CpuStat
+import com.github.kerubistan.kerub.model.dynamic.HostDynamic
 import com.github.kerubistan.kerub.model.dynamic.VirtualMachineDynamic
 import com.github.kerubistan.kerub.testHost
+import com.github.kerubistan.kerub.testLvmCapability
 import com.github.kerubistan.kerub.testVm
 import com.github.kerubistan.kerub.utils.toSize
 import io.github.kerubistan.kroki.size.GB
+import io.github.kerubistan.kroki.size.TB
 import io.github.kerubistan.kroki.time.now
 import org.junit.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class DataDiffKtTest {
 
 	data class Person(
-			val name : String,
-			val address : String? = null
+			val name: String,
+			val address: String? = null
 	)
 
 	@Test
 	fun diffNulls() {
-		assertNotNull(diff(Person("bob", null) , Person("bob", "Somewhere")))
-		assertNotNull(diff(Person("bob", "Somewhere") , Person("bob", null)))
+		assertNotNull(diff(Person("bob", null), Person("bob", "Somewhere")))
+		assertNotNull(diff(Person("bob", "Somewhere"), Person("bob", null)))
+	}
+
+	@Test
+	fun diffHostDyns() {
+		val diff = diff(
+				oldData = HostDynamic(
+						id = testHost.id,
+						storageStatus = listOf(
+								CompositeStorageDeviceDynamic(
+										id = testLvmCapability.id,
+										reportedFreeCapacity = 1.TB,
+										items = listOf(
+												CompositeStorageDeviceDynamicItem(
+														name = "/dev/sda", freeCapacity = 512.GB
+												),
+												CompositeStorageDeviceDynamicItem(
+														name = "/dev/sdb", freeCapacity = 512.GB
+												)
+										)
+								)
+						)
+				),
+				newData = HostDynamic(
+						id = testHost.id,
+						storageStatus = listOf(
+								CompositeStorageDeviceDynamic(
+										id = testLvmCapability.id,
+										reportedFreeCapacity = 600.GB,
+										items = listOf(
+												CompositeStorageDeviceDynamicItem(
+														name = "/dev/sda", freeCapacity = 512.GB
+												),
+												CompositeStorageDeviceDynamicItem(
+														name = "/dev/sdb", freeCapacity = 88.GB
+												)
+										)
+								)
+						)
+				)
+		)
+
+		assertTrue("Storage status by id is should not be in the diff " +
+				"as it is computed and therefore have @JsonIgnore") {
+			diff.none { it.property == HostDynamic::storageStatusById.name }
+		}
 	}
 
 	@Test
