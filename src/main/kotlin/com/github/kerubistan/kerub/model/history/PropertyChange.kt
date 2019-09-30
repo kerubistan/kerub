c55ccd8a2bee7ade1a0e1095b35c3faa10eaebc8
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-09-30 22:58:54 +0200

    shorter property names for property changes
    Bug-Url: https://github.com/kerubistan/kerub/issues/262
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/model/history/PropertyChange.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/model/history/PropertyChange.kt
@@ -1,9 +1,12 @@
 package com.github.kerubistan.kerub.model.history
 
+import org.codehaus.jackson.annotate.JsonProperty
 import java.io.Serializable
 
 data class PropertyChange(
 		val property: String,
+		@JsonProperty("old")
 		val oldValue: Serializable?,
+		@JsonProperty("new")
 		val newValue: Serializable?
 ) : Serializable
