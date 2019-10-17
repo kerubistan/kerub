f5c5301e4972b4fd17b94b662597225dc77d8efd
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-10-17 16:54:52 +0200

    operational state index re-ordered
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/OperationalStateIndex.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/OperationalStateIndex.kt
@@ -3,11 +3,16 @@ package com.github.kerubistan.kerub.planner
 import com.github.kerubistan.kerub.model.VirtualMachineStatus
 import com.github.kerubistan.kerub.model.dynamic.HostStatus
 import com.github.kerubistan.kerub.model.expectations.CloneOfStorageExpectation
+import com.github.kerubistan.kerub.model.expectations.StorageAvailabilityExpectation
 import com.github.kerubistan.kerub.model.expectations.VirtualMachineAvailabilityExpectation
 import com.github.kerubistan.kerub.utils.contains
 import com.github.kerubistan.kerub.utils.hasAny
 
-class OperationalStateIndex(val indexOf: OperationalState) {
+class OperationalStateIndex(private val indexOf: OperationalState) {
+
+	// -----
+	// hosts
+	// -----
 
 	val runningHosts by lazy { indexOf.hosts.values.filter { it.dynamic?.status == HostStatus.Up } }
 
@@ -28,25 +33,20 @@ class OperationalStateIndex(val indexOf: OperationalState) {
 		}.toMap()
 	}
 
-
 	val recyclingHosts by lazy {
 		indexOf.hosts.values
 				.filter { it.stat.recycling }
 				.associateBy { it.stat.id }
 	}
 
+	// ---
+	// VMs
+	// ---
+
 	val runningVms by lazy {
 		indexOf.vms.values.filter {
 			it.dynamic?.status == VirtualMachineStatus.Up
 		}
-	}
-
-	val allocatedStorage by lazy {
-		indexOf.vStorage.values.filter { it.dynamic?.allocations?.isNotEmpty() ?: false }
-	}
-
-	val storageCloneRequirement by lazy {
-		indexOf.vStorage.values.filter { it.stat.expectations.hasAny<CloneOfStorageExpectation>() }
 	}
 
 	val vmsThatMustStart by lazy {
@@ -58,5 +58,32 @@ class OperationalStateIndex(val indexOf: OperationalState) {
 		}
 	}
 
+	// ---------------
+	// virtual storage
+	// ---------------
+
+	val allocatedStorage by lazy {
+		indexOf.vStorage.values.filter { it.dynamic?.allocations?.isNotEmpty() ?: false }
+	}
+
+	val storageCloneRequirement by lazy {
+		indexOf.vStorage.values.filter { it.stat.expectations.hasAny<CloneOfStorageExpectation>() }
+	}
+
+	val virtualStorageNotAllocated by lazy {
+		indexOf.vStorage.values.filter { it.dynamic?.allocations?.isEmpty() ?: true }
+				.filter { storage ->
+					storage.stat.expectations.any {
+						it is StorageAvailabilityExpectation
+					}
+							||
+							vmsThatMustStart.any { vm ->
+								vm.stat.virtualStorageLinks.any { link ->
+									link.virtualStorageId == storage.stat.id
+								}
+							}
+				}.map { it.stat }
+
+	}
 
 }
