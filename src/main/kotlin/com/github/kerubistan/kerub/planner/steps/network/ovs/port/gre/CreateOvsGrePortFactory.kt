f3ea5b0d0d7b14d0597f18746ab0de03c318f346
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-11-26 17:34:03 +0100

    implementation and test for the left-behind ovs gre port factory
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/planner/steps/network/ovs/port/gre/CreateOvsGrePortFactory.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/planner/steps/network/ovs/port/gre/CreateOvsGrePortFactory.kt
@@ -1,24 +1,47 @@
 package com.github.kerubistan.kerub.planner.steps.network.ovs.port.gre
 
 import com.github.kerubistan.kerub.model.Expectation
+import com.github.kerubistan.kerub.model.config.OvsGrePort
+import com.github.kerubistan.kerub.model.config.OvsNetworkConfiguration
 import com.github.kerubistan.kerub.planner.OperationalState
 import com.github.kerubistan.kerub.planner.issues.problems.Problem
 import com.github.kerubistan.kerub.planner.steps.AbstractOperationalStepFactory
+import com.github.kerubistan.kerub.utils.hasAny
 import io.github.kerubistan.kroki.collections.concat
+import java.util.UUID.randomUUID
 import kotlin.reflect.KClass
 
 object CreateOvsGrePortFactory : AbstractOperationalStepFactory<CreateOvsGrePort>() {
-	override fun produce(state: OperationalState) = state.index.virtualNetworksNeeded.mapNotNull { neededNetworkId ->
+	override fun produce(state: OperationalState) = state.index.virtualNetworksNeeded.mapNotNull {
+		// for each network needed by any client workload
+		neededNetworkId ->
 
-		state.index.hostsByVirtualNetworks[neededNetworkId]?.map { host ->
+		val hosts = state.index.hostsByVirtualNetworks.getValue(neededNetworkId)
 
-			CreateOvsGrePort(
-					firstHost = host.stat,
-					secondHost = TODO(),
-					virtualNetwork = TODO(),
-					name = TODO()
-			)
-		}
+		hosts.map {
+			//for each host that has the network
+			sourceHost ->
+
+			hosts.filter {
+				// no to the same host, that is pointless of course
+				targetHost ->
+				targetHost.id != sourceHost.id
+						&& targetHost.config?.networkConfiguration?.none { networkConfig ->
+					networkConfig is OvsNetworkConfiguration
+							&& networkConfig.ports.hasAny<OvsGrePort> { it.remoteAddress == targetHost.stat.address }
+				} ?: false
+			}.map { targetHost ->
+
+				CreateOvsGrePort(
+						firstHost = sourceHost.stat,
+						secondHost = targetHost.stat,
+						virtualNetwork = state.vNet.getValue(neededNetworkId),
+						name = "gre-${randomUUID()}"
+				)
+
+			}
+
+		}.concat()
 
 	}.concat()
 
---
+++ src/test/kotlin/com/github/kerubistan/kerub/planner/steps/network/ovs/port/gre/CreateOvsGrePortFactoryTest.kt
@@ -0,0 +1,73 @@
+package com.github.kerubistan.kerub.planner.steps.network.ovs.port.gre
+
+import com.github.kerubistan.kerub.hostUp
+import com.github.kerubistan.kerub.model.config.HostConfiguration
+import com.github.kerubistan.kerub.model.config.OvsNetworkConfiguration
+import com.github.kerubistan.kerub.model.devices.NetworkAdapterType
+import com.github.kerubistan.kerub.model.devices.NetworkDevice
+import com.github.kerubistan.kerub.model.expectations.VirtualMachineAvailabilityExpectation
+import com.github.kerubistan.kerub.planner.OperationalState
+import com.github.kerubistan.kerub.planner.steps.AbstractFactoryVerifications
+import com.github.kerubistan.kerub.testHost
+import com.github.kerubistan.kerub.testOtherHost
+import com.github.kerubistan.kerub.testVirtualNetwork
+import com.github.kerubistan.kerub.testVm
+import com.github.kerubistan.kerub.vmUp
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+internal class CreateOvsGrePortFactoryTest : AbstractFactoryVerifications(CreateOvsGrePortFactory) {
+
+	@Test
+	fun produce() {
+		assertTrue("there are two hosts, both have the test network - let's offer creating a GRE port") {
+			val vm = testVm.copy(
+					devices = listOf(
+							NetworkDevice(
+									adapterType = NetworkAdapterType.virtio,
+									networkId = testVirtualNetwork.id
+							)
+					),
+					expectations = listOf(
+							VirtualMachineAvailabilityExpectation()
+					)
+			)
+			val steps = factory.produce(
+					OperationalState.fromLists(
+							hosts = listOf(testHost, testOtherHost),
+							hostDyns = listOf(hostUp(testHost), hostUp(testOtherHost)),
+							hostCfgs = listOf(
+									HostConfiguration(
+											id = testHost.id,
+											networkConfiguration = listOf(
+													OvsNetworkConfiguration(
+															virtualNetworkId = testVirtualNetwork.id
+													)
+											)
+									),
+									HostConfiguration(
+											id = testOtherHost.id,
+											networkConfiguration = listOf(
+													OvsNetworkConfiguration(
+															virtualNetworkId = testVirtualNetwork.id
+													)
+											)
+									)
+							),
+							virtualNetworks = listOf(testVirtualNetwork),
+							vms = listOf(vm),
+							vmDyns = listOf(
+									vmUp(vm, testHost)
+							)
+					))
+
+			steps.any { step ->
+				step is CreateOvsGrePort
+						&& step.firstHost == testHost
+						&& step.secondHost == testOtherHost
+						&& step.virtualNetwork == testVirtualNetwork
+			}
+
+		}
+	}
+}
