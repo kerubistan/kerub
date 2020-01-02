b729913da5354638a4b99488d7a7dc2e2d0d74ee
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-01-02 18:43:51 +0100

    removed empty class body
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/main/kotlin/com/github/kerubistan/kerub/model/config/OvsDataPort.kt
+++ src/main/kotlin/com/github/kerubistan/kerub/model/config/OvsDataPort.kt
@@ -3,5 +3,4 @@ package com.github.kerubistan.kerub.model.config
 import com.fasterxml.jackson.annotation.JsonTypeName
 
 @JsonTypeName("ovs-data-port")
-data class OvsDataPort(override val name: String) : OvsPort {
-}
+data class OvsDataPort(override val name: String) : OvsPort
