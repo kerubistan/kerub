0702c68ca8d630c0666f11e1e0ef82296e0d537f
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2022-11-08 16:32:15 +0100

    reflections api upgrade to clear warnings
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/kotlin/com/github/kerubistan/kerub/data/IgnoreDerivedPropertiesTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/data/IgnoreDerivedPropertiesTest.kt
@@ -2,26 +2,26 @@ package com.github.kerubistan.kerub.data
 
 import org.junit.Test
 import org.reflections.Reflections
-import org.reflections.scanners.SubTypesScanner
+import org.reflections.scanners.Scanners
 import kotlin.reflect.full.declaredMemberProperties
 
 class IgnoreDerivedPropertiesTest {
 	@Test
 	fun check() {
-		val reflections = Reflections("com.github.kerubistan.kerub.model", SubTypesScanner(false))
-		reflections.allTypes.map { Class.forName(it).kotlin }
-				.filterNot {
-					(it.java.name?.contains("Test") ?: true)
-							|| (it.java.name?.contains("DefaultImpls") ?: true)
-							|| (it.java.name?.contains("Companion") ?: true)
-							|| (it.java.name?.endsWith("Kt") ?: true)
+		val reflections = Reflections("com.github.kerubistan.kerub.model", Scanners.SubTypes)
+		reflections.getAll(Scanners.SubTypes).map { Class.forName(it).kotlin }
+			.filterNot {
+				(it.java.name.contains("Test"))
+						|| (it.java.name.contains("DefaultImpls"))
+						|| (it.java.name.contains("Companion"))
+						|| (it.java.name.endsWith("Kt"))
+			}
+			.sortedBy { it.qualifiedName }
+			.forEach { clzz ->
+				println(clzz.qualifiedName)
+				clzz.declaredMemberProperties.forEach {
+					println("	- ${it.name}")
 				}
-				.sortedBy { it.qualifiedName }
-				.forEach { clzz ->
-					println(clzz.qualifiedName)
-					clzz.declaredMemberProperties.forEach {
-						println("	- ${it.name}")
-					}
-				}
+			}
 	}
 }
--- src/test/kotlin/com/github/kerubistan/kerub/data/ImmutableModelTest.kt
+++ src/test/kotlin/com/github/kerubistan/kerub/data/ImmutableModelTest.kt
@@ -3,22 +3,22 @@ package com.github.kerubistan.kerub.data
 import org.apache.commons.beanutils.PropertyUtils
 import org.junit.Test
 import org.reflections.Reflections
-import org.reflections.scanners.SubTypesScanner
+import org.reflections.scanners.Scanners
 import kotlin.test.assertTrue
 
 class ImmutableModelTest {
 	// check that model classes are immutables (no setters ever)
 	@Test
 	fun check() {
-		val reflections = Reflections("com.github.kerubistan.kerub.model", SubTypesScanner(false))
-		reflections.allTypes.map { Class.forName(it).kotlin }
-				.filter { !(it.java.name?.contains("Test") ?: true) }
-				.forEach {clzz ->
-					PropertyUtils.getPropertyDescriptors(clzz.java).forEach { prop ->
-						assertTrue("${clzz.qualifiedName}/${prop.name} should be immutable") {
-							prop.writeMethod == null
-						}
+		val reflections = Reflections("com.github.kerubistan.kerub.model", Scanners.SubTypes)
+		reflections.getAll(Scanners.SubTypes).map { Class.forName(it).kotlin }
+			.filter { !(it.java.name.contains("Test")) }
+			.forEach { clzz ->
+				PropertyUtils.getPropertyDescriptors(clzz.java).forEach { prop ->
+					assertTrue("${clzz.qualifiedName}/${prop.name} should be immutable") {
+						prop.writeMethod == null
 					}
 				}
+			}
 	}
 }
